91bb8d5da4a9f5a98b974a4a321504b95a811f42
nathanwenger <nedwenger@gmail.com>
2018-07-11 15:24:30 -0700

    Changed synthetic event controlling stars

--- src/index.js
+++ src/index.js
@@ -422,11 +422,16 @@ class App extends React.Component {
       	</div>
       	<section className="rating-widget">
       	  <div className="rating-stars center-align">
-      			<a className={this.state.ratingLock > 0 ? 'star-active-lock' : this.state.rating > 0 ? 'star-active' : 'star'} onMouseEnter={() => this.setState({rating:1})} onMouseOut={() => this.setState({rating:0})} onClick={(e) => {this.setState({ratingLock:1}); e.stopPropagation()}}><Icon>star</Icon></a>
-            <a className={this.state.ratingLock > 1 ? 'star-active-lock' : this.state.rating > 1 ? 'star-active' : 'star'} onMouseEnter={() => this.setState({rating:2})} onMouseOut={() => this.setState({rating:0})} onClick={(e) => {this.setState({ratingLock:2}); e.stopPropagation()}}><Icon>star</Icon></a>
-            <a className={this.state.ratingLock > 2 ? 'star-active-lock' : this.state.rating > 2 ? 'star-active' : 'star'} onMouseEnter={() => this.setState({rating:3})} onMouseOut={() => this.setState({rating:0})} onClick={(e) => {this.setState({ratingLock:3}); e.stopPropagation()}}><Icon>star</Icon></a>
-            <a className={this.state.ratingLock > 3 ? 'star-active-lock' : this.state.rating > 3 ? 'star-active' : 'star'} onMouseEnter={() => this.setState({rating:4})} onMouseOut={() => this.setState({rating:0})} onClick={(e) => {this.setState({ratingLock:4}); e.stopPropagation()}}><Icon>star</Icon></a>
-            <a className={this.state.ratingLock > 4 ? 'star-active-lock' : this.state.rating > 4 ? 'star-active' : 'star'} onMouseEnter={() => this.setState({rating:5})} onMouseOut={() => this.setState({rating:0})} onClick={(e) => {this.setState({ratingLock:5}); e.stopPropagation()}}><Icon>star</Icon></a>
+      			<a className={this.state.ratingLock > 0 ? 'star-active-lock' : this.state.rating > 0 ? 'star-active' : 'star'} onMouseOver={() => this.setState({rating:1})} onMouseOut={() => this.setState({rating:0})}
+              onClick={(e) => {this.setState({ratingLock:1}); e.stopPropagation()}}><Icon>star</Icon></a>
+            <a className={this.state.ratingLock > 1 ? 'star-active-lock' : this.state.rating > 1 ? 'star-active' : 'star'} onMouseOver={() => this.setState({rating:2})} onMouseOut={() => this.setState({rating:0})}
+              onClick={(e) => {this.setState({ratingLock:2}); e.stopPropagation()}}><Icon>star</Icon></a>
+            <a className={this.state.ratingLock > 2 ? 'star-active-lock' : this.state.rating > 2 ? 'star-active' : 'star'} onMouseOver={() => this.setState({rating:3})} onMouseOut={() => this.setState({rating:0})}
+              onClick={(e) => {this.setState({ratingLock:3}); e.stopPropagation()}}><Icon>star</Icon></a>
+            <a className={this.state.ratingLock > 3 ? 'star-active-lock' : this.state.rating > 3 ? 'star-active' : 'star'} onMouseOver={() => this.setState({rating:4})} onMouseOut={() => this.setState({rating:0})}
+              onClick={(e) => {this.setState({ratingLock:4}); e.stopPropagation()}}><Icon>star</Icon></a>
+            <a className={this.state.ratingLock > 4 ? 'star-active-lock' : this.state.rating > 4 ? 'star-active' : 'star'} onMouseOver={() => this.setState({rating:5})} onMouseOut={() => this.setState({rating:0})}
+              onClick={(e) => {this.setState({ratingLock:5}); e.stopPropagation()}}><Icon>star</Icon></a>
       	  </div>
       	  <div className="message-box" onClick={(e) => e.stopPropagation()}>
       		<textarea placeholder="Enter your review here." className="sttv-textarea"></textarea>
