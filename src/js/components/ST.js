52b24e21f16b778289e121c0165ec19695511e32
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-22 14:36:58 -0700

    Basic testing

--- src/js/components/ST.js
+++ src/js/components/ST.js
@@ -1,30 +1,38 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
+import {BrowserRouter, Switch, Route, Link, Redirect} from 'react-router-dom'
+import Header from './header/Header'
+import Sidebar from './sidebar/Sidebar'
 
 export default class ST extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            activeClass : 'loading'
+            loading : true
         }
     }
 
     componentDidMount() {
         setTimeout(() => {
             this.visible()
-        },10000)
+        },5000)
     }
 
-    hidden() {
-        this.setState({activeClass : 'loading'})
+    loading() {
+        this.setState({loading : true})
     }
 
-    visible() {
-        this.setState({activeClass : 'active'})
+    active() {
+        this.setState({loading : false})
     }
 
     render() {
         return (
-            <div id="stAppMain" className={this.state.activeClass}>Coming soon</div>
+            <div id="stAppInner" className={this.state.loading ? 'loading' : 'active'}>
+                <Header />
+                <Sidebar />
+            </div>
         )
     }
 }
--- src/js/entry.js
+++ src/js/entry.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {TransitionGroup, CSSTransition} from "react-transition-group"
-import {BrowserRouter, Switch, Route, Link, Redirect} from "react-router-dom"
 import ST from './components/ST'
 
 const stApp = document.getElementById("stApp")
