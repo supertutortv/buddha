2bf6e663971c0b2efa0ad413b069be674bccb13a
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-22 20:33:54 -0700

    ryjmyr,

--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -16,9 +16,9 @@ export default class Main extends React.Component {
         return(
             <main id="stAppStage">
                 <Switch>
+                    <Route path='/login' component={Login} />
                     <Route path='/' render={() => <Redirect to="/dashboard" />} />
                     <Route path='/dashboard' component={Login} />
-                    <Route path='/login' component={Login} />
                     <Route path="/*" component={Four04} />
                 </Switch>
             </main>
