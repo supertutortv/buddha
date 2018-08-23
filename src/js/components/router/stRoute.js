8bc3cbaa665100e1264d6817fbc29acc309e2556
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-23 14:20:55 -0700

    genehn

--- src/js/components/router/stRoute.js
+++ src/js/components/router/stRoute.js
@@ -4,15 +4,19 @@ import {Route} from 'react-router-dom'
 export default class STRoute extends Component {
     constructor(props) {
         super(props)
+        console.log(this.props)
     }
 
-    renderMergedProps() {
-
+    renderMergedProps(component,...rest) {
+        const final = Object.assign({},...rest)
+        return (
+            React.createElement(component, final)
+        )
     }
 
     render() {
         return (
-            <Route {...this.props} />
+            <Route  />
         )
     }
 }
