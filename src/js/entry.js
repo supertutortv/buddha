8f495cac86cfeb49e0a99785a2b08fbc7168a845
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-22 18:14:09 -0700

    updates

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter,Route} from 'react-router-dom'
 import ST from './components/ST'
 import allYourBase from './components/allYourBase'
 
