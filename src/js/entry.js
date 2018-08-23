9b2ee22d807d33324a8af04134f626c50d9b3aec
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-22 18:15:58 -0700

    iyvlvyhgr

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter,Route} from 'react-router-dom'
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
 import ST from './components/ST'
 import allYourBase from './components/allYourBase'
 
 ReactDOM.render(
     <BrowserRouter>
-        <Route path='/all-your-base-are-belong-to-us' component={allYourBase} />
-        <Route path='/' component={ST} />
+        <Switch>
+            <Route path='/all-your-base-are-belong-to-us' component={allYourBase} />
+            <Route path='/' component={ST} />
+        </Switch>
     </BrowserRouter>,
     document.getElementById("stApp")
 )
