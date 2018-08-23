493dd45dd87bec2f9f1b9535df40c44a04f846d7
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-23 13:03:13 -0700

    ernern

--- src/js/components/pages/allYourBase.js
+++ src/js/components/pages/allYourBase.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default class allYourBase extends React.Component {
     render() {
         return(
-            <img style={{width:'75%'}} src="https://upload.wikimedia.org/wikipedia/en/0/03/Aybabtu.png" />
+            <div style={{width:'100%',height:'100%',textAlign:'center'}}>
+                <img style={{width:'auto',height:'100%'}} src="https://upload.wikimedia.org/wikipedia/en/0/03/Aybabtu.png" />
+            </div>
         )
     }
 }
