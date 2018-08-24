cefb50f39694e0b07a51736eeadbab5e50cc18c7
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-23 19:26:31 -0700

    4umryjmrjym

--- src/js/components/ST.js
+++ src/js/components/ST.js
@@ -18,7 +18,7 @@ export default class ST extends React.Component {
 
         this.verifySession((d) => {
             this.setState({
-                loggedIn : d.data,
+                loggedIn : !d.data,
                 loading : false
             },() => this.loading())
         })
