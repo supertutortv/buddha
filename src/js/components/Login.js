e6422569677598a26bfb33603d7cf14c89178350
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-25 16:12:48 -0700

    fb efgnegrn

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -8,8 +8,7 @@ export default class Login extends React.Component {
         super(props)
         this.state = {
             username : '',
-            password : '',
-            skipLogin : false
+            password : ''
         }
     }
 
@@ -22,28 +21,34 @@ export default class Login extends React.Component {
     }
 
     render() {
-        return this.state.skipLogin ? (<Redirect to='/dashboard'/>) : (
-            <form id="stLoginWrapper" className="stFormWrapper row">
-                <div className="stOverlay"></div>
-                <div id="stLoginHeader" className="stFormHeader col s12">
-                    <h2>Sign into your account</h2>
-                    <span>You can access all of your test prep courses, as well as all of your account information, by logging in below.</span>
-                </div>
-                <div id="stLoginCredentials" className="col s12">
-                    <div className="input-field col s12">
-                        <input className="browser-default validate email" type="email" name="st-username" placeholder="Email Address" onBlur={this.setState}/>
-                    </div>
-                    <div className="input-field col s12">
-                        <input className="browser-default validate" type="password" name="st-password" placeholder="Password" onBlur={this.setState}/>
-                    </div>
-                </div>
-                <div className="stForgotBlock col s12">
-                    <span><a href="/">Forgot your password?</a></span>
-                </div>
-                <div className="stFormButtons col s12">
-                    <button className="stFormButton pmt-button btn waves-effect waves-light">Login</button>
-                </div>
-            </form>
+        return (
+            <GlobalState.Consumer>
+                {gstate => {
+                    return (gstate.loggedIn) ? <Redirect to='/dashboard'/> : (
+                        <form id="stLoginWrapper" className="stFormWrapper row">
+                            <div className="stOverlay"></div>
+                            <div id="stLoginHeader" className="stFormHeader col s12">
+                                <h2>Sign into your account</h2>
+                                <span>You can access all of your test prep courses, as well as all of your account information, by logging in below.</span>
+                            </div>
+                            <div id="stLoginCredentials" className="col s12">
+                                <div className="input-field col s12">
+                                    <input className="browser-default validate email" type="email" name="st-username" placeholder="Email Address" onBlur={this.setState}/>
+                                </div>
+                                <div className="input-field col s12">
+                                    <input className="browser-default validate" type="password" name="st-password" placeholder="Password" onBlur={this.setState}/>
+                                </div>
+                            </div>
+                            <div className="stForgotBlock col s12">
+                                <span><a href="/">Forgot your password?</a></span>
+                            </div>
+                            <div className="stFormButtons col s12">
+                                <button className="stFormButton pmt-button btn waves-effect waves-light">Login</button>
+                            </div>
+                        </form>
+                    )
+                }}
+            </GlobalState.Consumer>
         )
     }
 }
