8812bbe09f234a4064ac33a993d0a97ebc285beb
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-25 13:03:39 -0700

    eyeynetyj

--- src/js/classes/st.js
+++ src/js/classes/st.js
@@ -1,5 +1,23 @@
-import config from '../utilities/config'
 import loading from './modules/loading'
+
+const config = {
+    env : 'staging',
+    production : {
+        stripe : {
+            publicKey : 'pk_live_ns71ckv2wHyLaP7CCyxp3DS7'
+        }
+    },
+    staging : {
+        stripe : {
+            publicKey : 'pk_test_aF7R0He4Yt4OYgAq3iERimgn'
+        }
+    },
+    development : {
+        stripe : {
+            publicKey : 'pk_test_aF7R0He4Yt4OYgAq3iERimgn'
+        }
+    }
+}
 
 export const stripe = config[config.env].stripe
 
