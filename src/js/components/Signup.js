087109f208f83567416d0b54dba9dfbb0d66c2f8
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-25 19:26:49 -0700

    rynerhjey

--- src/js/components/Signup.js
+++ src/js/components/Signup.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import {Switch,Route,Redirect} from 'react-router-dom'
+import STStrippedWrapper from './STStrippedWrapper'
 
 export default class Signup extends React.Component {
     constructor(props) {
         super(props)
+        this.init = false
+        this.state = {}
+        console.log(this.props)
     }
     render() {
         return(
-            <div>Signup page</div>
+            <STStrippedWrapper>
+                <div>Signup page</div>
+            </STStrippedWrapper>
         )
     }
 }
