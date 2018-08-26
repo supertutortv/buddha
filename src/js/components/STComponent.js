e7c7f563d93d731fbef0065542dc602d5d0ca4b9
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-25 17:31:09 -0700

    thwrtheryjnyr

--- src/js/components/STComponent.js
+++ src/js/components/STComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class STComponent extends React.Component {
     constructor(props) {
         super(props)
-        console.log(props)
+        console.log(this.props)
     }
     render() {}
 }
