15c73415f6249a03b81698c7e261987e06059cba
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-25 19:03:37 -0700

    rthwrnrn

--- src/js/components/STStrippedWrapper.js
+++ src/js/components/STStrippedWrapper.js
@@ -4,15 +4,10 @@ export default class STStrippedWrapper extends React.Component {
     render() {
         return (
             <section id="stStripped" className="row">
-                <div id="stStrippedInset" className="col s12 l8 offset-l2">
+                <div id="stStrippedInset" className="col s12 m8 l6 offset-m2 offset-l3">
                     <div id="stStrippedSpacer" className="row z-depth-4">
                         <header className="stBordered row">
                             <div id="stStrippedInsetDarkHeader" className="col s12">
-                                <img src="http://localhost:8888/sttvroot/wp-content/themes/sttvsite/i/sttv_logo_contrast.png" />
-                            </div>
-                        </header>
-                        <header className="stBoxed row">
-                            <div id="stStrippedInsetLightHeader" className="col s12">
                                 <img src="http://localhost:8888/sttvroot/wp-content/themes/sttvsite/i/sttv_logo_contrast.png" />
                             </div>
                         </header>
