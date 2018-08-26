13d24e4c476d63f0fec4cba55c129f94b4b9eaf1
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-26 14:56:16 -0700

    ryhry3jhty

--- src/js/components/signup/steps.js
+++ src/js/components/signup/steps.js
@@ -6,10 +6,10 @@ export const plans = (plans) => {
     return (
 <div id="step-2" class="stFormStep row">
     <div class="stFormHeader col s12">
-        <h2>Cool, now select a plan.</h2>
+        <h2>Select a plan.</h2>
         <span>All plans come with a 5 day free trial. <strong>NOTE:</strong> Your card will not be charged until your trial period is over, and you're free to cancel at any time. If your course comes with free books, they will not ship until your trial has expired.</span>
     </div>
-    <div id="stSignupPlans" class="stFormBody col s12">{plans}</div>
+    <div id="stSignupPlans" class="stFormBody col s12">{}</div>
 </div>
 )}
 
