3fd39ae60334811358624c2c33f24aeaa094d4a5
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-26 13:03:10 -0700

    khgkhcchg

--- src/js/components/STStrippedWrapper.js
+++ src/js/components/STStrippedWrapper.js
@@ -8,13 +8,13 @@ export default class STStrippedWrapper extends React.Component {
                     <div id="stStrippedSpacer" className="row z-depth-4">
                         <header className="stBordered row">
                             <div id="stStrippedInsetDarkHeader" className="col s12">
-                                <img src="http://localhost:8888/sttvroot/wp-content/themes/sttvsite/i/sttv_logo_contrast.png" />
+                                <img src="/assets/img/sttv_logo_contrast.png" />
                             </div>
                         </header>
                         {this.props.children}
                         <div id="stFormErrors" className="row"></div>
                         <footer className="row">
-                            <img src="http://localhost:8888/sttvroot/wp-content/themes/sttvsite/i/supertutortv-students.png" />
+                            <img src="/assets/img/supertutortv-students.png" />
                         </footer>
                     </div>
                 </div>
