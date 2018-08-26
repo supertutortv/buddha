3041fa13e6b622cfc6accdbe443def9c546a9b73
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-25 19:31:22 -0700

    utkd,fgj,jg,

--- src/js/components/Signup.js
+++ src/js/components/Signup.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Switch,Route,Redirect} from 'react-router-dom'
 import STStrippedWrapper from './STStrippedWrapper'
+import * as _st from '../classes/st'
 
 export default class Signup extends React.Component {
     constructor(props) {
@@ -9,6 +10,15 @@ export default class Signup extends React.Component {
         this.state = {}
         console.log(this.props)
     }
+
+    componentWillUnmount() {
+        _st.bodyClass('signup')
+    }
+
+    componentDidMount() {
+        _st.bodyClass('signup')
+    }
+
     render() {
         return(
             <STStrippedWrapper>
