95fa9e377d8f3eb9b2311b8b783161b986b99a8e
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-26 15:12:28 -0700

    t5hthwtrh

--- src/js/components/Signup.js
+++ src/js/components/Signup.js
@@ -11,6 +11,18 @@ export default class Signup extends React.Component {
         this.state = {}
     }
 
+    plans() {
+        console.log(this)
+        return (
+        <div id="step-2" class="stFormStep row">
+            <div class="stFormHeader col s12">
+                <h2>Select a plan.</h2>
+                <span>All plans come with a 5 day free trial. <strong>NOTE:</strong> Your card will not be charged until your trial period is over, and you're free to cancel at any time. If your course comes with free books, they will not ship until your trial has expired.</span>
+            </div>
+            <div id="stSignupPlans" class="stFormBody col s12">{}</div>
+        </div>
+    )}
+
     componentWillUnmount() {
         _st.bodyClass('signup')
     }
@@ -27,7 +39,7 @@ export default class Signup extends React.Component {
         return(
             <STStrippedWrapper>
                 <Switch>
-                    <Route path='/signup/:step' render={(d) => (typeof steps[d.match.params.step] !== 'undefined') ? steps[d.match.params.step].bind(this,d,this) : <Redirect to='/notfound' /> } />
+                    <Route path='/signup/:step' component={this.plans} />
                     <Route exact path='/signup' render={(d) => {return steps.plans(d)}} />
                 </Switch>
             </STStrippedWrapper>
--- src/js/components/signup/steps.js
+++ src/js/components/signup/steps.js
@@ -2,20 +2,6 @@ import React from 'react'
 import {Redirect} from 'react-router-dom'
 import * as _st from '../../classes/st'
 
-export const plans = (plans,x) => {
-    x.renderStep()
-    var render = false
-    console.log(plans,x)
-    return (
-<div id="step-2" class="stFormStep row">
-    <div class="stFormHeader col s12">
-        <h2>Select a plan.</h2>
-        <span>All plans come with a 5 day free trial. <strong>NOTE:</strong> Your card will not be charged until your trial period is over, and you're free to cancel at any time. If your course comes with free books, they will not ship until your trial has expired.</span>
-    </div>
-    <div id="stSignupPlans" class="stFormBody col s12">{}</div>
-</div>
-)}
-
 export const account = () => <div>account</div>
 export const billing = () => <div>billing</div>
 export const shipping = () => <div>shipping</div>
