f830cdeb43ddd1c10b76f1f054a1ea4ddaa4eba1
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-27 12:03:22 -0700

    t3hrthy3hjyt

--- src/js/components/signup/initSession.js
+++ src/js/components/signup/initSession.js
@@ -1,11 +1,18 @@
 import * as _st from '../../classes/st'
 
 export default function initSession(plan) {
-    var planId = (typeof plan === 'string') ? plan : plan.target.id.replace('stPlan-','')
-    console.log(planId)
-    this.setState({
-        step: 1,
-        init: true
+    var planId = (typeof plan === 'string') ? plan : plan.target.id.replace('stPlan-',''),
+        thePlan = {}
+
+    _st.plans.some((obj) => {
+        if (obj.id === planId || obj.name === planId)
+            return thePlan = obj
     })
+
+    if (Object.keys(thePlan).length)
+        this.setState({
+            step: 1,
+            init: true
+        })
     return null
 }
