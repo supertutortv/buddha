6ec209f20608de00dff92c1373a3b46c147cad68
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-27 11:52:22 -0700

    rgqergwegwetg

--- src/js/components/signup/initSession.js
+++ src/js/components/signup/initSession.js
@@ -1,7 +1,7 @@
 import * as _st from '../../classes/st'
 
 export default function initSession(plan) {
-    var planId = (typeof plan === 'string') ? plan : plan.target.id
+    var planId = (typeof plan === 'string') ? plan : plan.target.id.replace('stPlan-','')
     console.log(planId)
     this.setState({
         step: 1,
