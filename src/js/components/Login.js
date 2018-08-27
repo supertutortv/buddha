ae4cbe01461d11ddb439ea7ac0f79478e2e6d1ad
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-27 15:42:31 -0700

    rtwbwrgwrgn

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -3,7 +3,7 @@ import { GlobalState } from '../utilities/StateContext'
 import STStrippedWrapper from './STStrippedWrapper'
 import * as _st from '../classes/st'
 
-export const login = () => {
+export default login = () => {
     return (
         <STStrippedWrapper>
             <form id="stLoginWrapper" className="stFormWrapper row" onSubmit={this.submit}>
