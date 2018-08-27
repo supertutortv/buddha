f93903bd9370826db30d86aaf7e0a8ef12bc4ad4
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-27 11:30:09 -0700

    ejejrj

--- src/js/components/signup/initSession.js
+++ src/js/components/signup/initSession.js
@@ -2,9 +2,8 @@ import * as _st from '../../classes/st'
 
 export default function initSession(plan) {
     var plans = _st.plans
-    this.setState({
+    return this.setState({
         step: 1,
         init: true
     })
-    return null
 }
