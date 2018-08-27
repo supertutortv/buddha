d1b21fa9aee88086f82eec7753e995d9190c2f8f
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-27 15:05:41 -0700

    trwrnehn

--- src/js/components/allYourBase.js
+++ src/js/components/allYourBase.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { GlobalState } from '../utilities/StateContext'
 
-export default allYourBase = props => {
-    return(
-        <GlobalState.Consumer>
-            {context => {
-                context.bodyClass('allYourBase')
-                return (
-                    <div style={{width:'100%',height:'100%',textAlign:'center',backgroundColor:'black'}}>
-                        <img style={{width:'auto',height:'100%'}} src="https://upload.wikimedia.org/wikipedia/en/0/03/Aybabtu.png" />
-                    </div>
-                )
-            }}
-        </GlobalState.Consumer>
-    )
+export default class allYourBase extends React.Component {
+    render() {
+        return(
+            <GlobalState.Consumer>
+                {context => {
+                    context.bodyClass('allYourBase')
+                    return (
+                        <div style={{width:'100%',height:'100%',textAlign:'center',backgroundColor:'black'}}>
+                            <img style={{width:'auto',height:'100%'}} src="https://upload.wikimedia.org/wikipedia/en/0/03/Aybabtu.png" />
+                        </div>
+                    )
+                }}
+            </GlobalState.Consumer>
+        )
+    }
 }
