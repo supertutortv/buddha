1b0e12c9a7364d0f8f9fc3cc485e6cb578077af5
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-28 11:57:36 -0700

    btwbwrt

--- src/js/components/signup/createAccount.js
+++ src/js/components/signup/createAccount.js
@@ -2,5 +2,5 @@ import * as _st from '../../classes/st'
 
 export default function createAccount(e) {
     e.preventDefault()
-    console.log(this.state.customer.account)
+    console.log(this.state)
 }
