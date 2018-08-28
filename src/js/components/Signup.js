088e17aee6d8ba80780d8e2268ad719283abe05a
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-28 14:18:59 -0700

    gwrghrtwhwrt

--- src/js/components/Signup.js
+++ src/js/components/Signup.js
@@ -66,8 +66,9 @@ export default class Signup extends React.Component {
     updateInp(e) {
         var el = e.target
         this.setState(prev => {
-            var params = el.name.split('|')
-            console.log({[params[0]] : {...prev.session[params[0]]}})
+            var params = el.name.split('|'),
+                oldObj = newObj = {}
+            console.log({[params[0]] : {...prev.session[params[0]], account:0}})
             /* return Object.assign(prev.session,el.name.split('|').reduce((obj,val,i,arr) => {
                 return (i+1 === arr.length) ? el.value : obj[val]
             },prev.session)) */
