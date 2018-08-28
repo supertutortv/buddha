8ada1372a5e15324e8c7886f38a38811b401da17
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-28 13:36:09 -0700

    rhryjetyjtyj

--- src/js/components/Signup.js
+++ src/js/components/Signup.js
@@ -69,6 +69,7 @@ export default class Signup extends React.Component {
                 return {[val] : (i+1 === arr.length) ? e.target.value : obj}
             },{})
             console.log(prev)
+            return {}
         })
     }
 
