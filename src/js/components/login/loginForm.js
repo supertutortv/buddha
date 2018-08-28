4b40389189fb17985b786a9634e76540cfdf069a
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-27 17:29:09 -0700

    rthwrheryh

--- src/js/components/login/loginForm.js
+++ src/js/components/login/loginForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Redirect } from 'react-router-dom'
+import { Redirect, Link } from 'react-router-dom'
 import * as _st from '../../classes/st'
 
 export default function loginForm() {
@@ -19,7 +19,7 @@ export default function loginForm() {
                 </div>
             </div>
             <div className="stForgotBlock col s12">
-                <span><a onClick={() => <Redirect to='/login/lostpw' />}>Forgot your password?</a></span>
+                <span><Link to='/login/lostpw'>Forgot your password?</Link></span>
             </div>
             <div className="stFormButtons col s12">
                 <button className="stFormButton pmt-button btn waves-effect waves-light">Login</button>
