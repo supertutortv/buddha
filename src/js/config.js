15db6f835b2c18e163763530336ecc4331f1fbd4
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-31 15:48:24 -0700

    Zm4swYBAUV2zbK7cIJ4f

---
+++ src/js/config.js
@@ -0,0 +1,13 @@
+export default config = {
+    env : 'development',
+    production : {
+        stripe : {
+            publicKey : 'pk_live_ns71ckv2wHyLaP7CCyxp3DS7'
+        }
+    },
+    development : {
+        stripe : {
+            publicKey : 'pk_test_aF7R0He4Yt4OYgAq3iERimgn'
+        }
+    }
+}
