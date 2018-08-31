37f56fa90f5563b33c1914677baaf9e7159c1993
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-31 16:48:43 -0700

    zFPq4mu4muDNf3CT1NsF

--- src/js/_st.js
+++ src/js/_st.js
@@ -69,7 +69,7 @@ _st.prototype = {
         console.log(this)
     },
     auth : {
-        _auth : (() => this)(),
+        _auth : (function() {return this})(),
         authFunc : function() {
             console.log(this._auth)
         }
