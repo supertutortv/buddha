7bb6de9c7a7ac66b38561d34e0a245790acb625d
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-31 20:41:53 -0700

    CqYFvfZtTQwv5DLxvG3P

--- src/js/_st.js
+++ src/js/_st.js
@@ -61,7 +61,14 @@ _st.prototype = {
         loggedIn: null,
         data: {}
     },
-    ...loading,
+    get loading() {
+        return this._state.loading
+    },
+    set loading(val) {
+        this._state.loading = val
+        console.log(_st.ROOT)
+        document.getElementById('stApp').classList.toggle('loading',this._state.loading)
+    },
     ...bodyClass
 }
 
