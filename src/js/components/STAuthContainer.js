8adc98ffbbe7d25148868dd812fabbd65ef27155
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-01 10:36:46 -0700

    Tq7QjYaAE2prnVLpwBTq

--- src/js/components/STAuthContainer.js
+++ src/js/components/STAuthContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import STStrippedWrapper from './STStrippedWrapper'
+import Main from './Login'
 import Main from './Main'
 
 export default class STAuthContainer extends React.Component {
