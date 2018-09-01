03d8f849ecee5673f40d450aeab6bdc2a98424b1
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-01 09:39:49 -0700

    Zz2woONIfy733enO9OK0

--- src/js/utilities/auth.js
+++ src/js/utilities/auth.js
@@ -1,6 +1,6 @@
 import { post } from './http'
 
-export default auth = {
+export default {
     verify : (cb) => post('https://api.supertutortv.com/v2/auth/verify',{},cb),
     token : (dt,cb) => post('https://api.supertutortv.com/v2/auth/token',dt,cb),
     logout : (cb) => post('https://api.supertutortv.com/v2/auth/logout',{},cb)
