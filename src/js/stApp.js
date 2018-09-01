1bca0fb849421f10c2c6e42031a62417350fc09a
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-01 14:03:37 -0700

    YxHwnpKZkxMOR5eBMsrk

--- src/js/stApp.js
+++ src/js/stApp.js
@@ -12,6 +12,7 @@ window._st = _st
 ReactDOM.render( 
     <BrowserRouter>
         <Switch>
+            {_st.loading = true}
             <Route exact path='/all-your-base-are-belong-to-us' component={allYourBase} />
             <Route exact path='/signup' component={Signup} />
             <STSecured path='/' component={Main} />
