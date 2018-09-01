b94f01f087cbc6dfe079a2333a421997357f78e8
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-01 13:54:24 -0700

    Fe1P1xF73f89qErf62pQ

--- src/js/components/STSecured.js
+++ src/js/components/STSecured.js
@@ -6,7 +6,7 @@ export default class STSecured extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            loggedIn: _st.loggedIn
+            loggedIn: null
         }
     }
 
