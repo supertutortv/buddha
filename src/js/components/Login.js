41becbb88a1b66876c18f2958f8e3356895664e9
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-03 11:29:40 -0700

    lZ4vT4lQmwGZ0be32Tdd

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -1,7 +1,28 @@
 import React from 'react'
 import STStrippedWrapper from './STStrippedWrapper'
-import LoginForm from './login/LoginForm'
 import ResetPassword from './login/ResetPassword'
+
+const LoginForm = ({setLoginState, lostPwGo}) =>
+    (<div>
+        <div id="stLoginHeader" className="stFormHeader col s12">
+            <h2>Welcome! Please sign in.</h2>
+            <span>You can access all of your test prep courses, as well as all of your account information, by logging in below.</span>
+        </div>
+        <div id="stLoginCredentials" className="col s12">
+            <div className="input-field col s12">
+                <input className="browser-default validate email" type="email" name="username" placeholder="Email Address" onBlur={setLoginState}/>
+            </div>
+            <div className="input-field col s12">
+                <input className="browser-default validate" type="password" name="password" placeholder="Password" onBlur={setLoginState}/>
+            </div>
+        </div>
+        <div className="stForgotBlock col s12">
+            <span><a onClick={lostPwGo}>Forgot your password?</a></span>
+        </div>
+        <div className="stFormButtons col s12">
+            <button className="stFormButton pmt-button btn waves-effect waves-light">Login</button>
+        </div>
+    </div>)
 
 export default class Login extends React.Component {
     constructor(props) {
