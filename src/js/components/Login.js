dd7ae9058f21b46cc03c2c205a6e2914fc4ff484
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-04 10:43:29 -0700

    VO4JjKNyTUiUv7UwfE49

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -99,11 +99,14 @@ export default class Login extends React.Component {
         e.preventDefault()
         _st.loading = true
         _st.auth.submit(this.props.location.pathname,this.state.creds,(d) => {
-            console.log(d)
-            /* if (d.code === '') return console.log(d)
-            if (d.code === 'login_success') {
-                this.props.setLoggedIn()
-            } */
+            if (d.code === 'login_fail') return this.setState({
+                    error: {
+                        id: d.code,
+                        message: d.message
+                    }
+                })
+            else
+                return this.props.setLoggedIn()
         })
     }
 
