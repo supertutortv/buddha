2affc468f000e87044439ef3a69c78042ff75b88
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-04 14:12:18 -0700

    KO261ytw8A2Iv608gGcH

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,68 @@
-var path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const globImporter = require('node-sass-glob-importer');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const globImporter = require('node-sass-glob-importer')
 
-module.exports = {
-  entry: ['babel-polyfill', './src/sass/stApp.sass','./src/js/stApp.js'],
-  output: {
-    path: path.resolve(__dirname, 'web'),
-    filename: './assets/js/stApp.js',
-    publicPath: '/'
-  },
-  devServer: {
-    publicPath: '/',
-    port: 8888
-  },
-  externals: {
-    materialize: 'M'
-  },
-  module: {
-    rules: [
-      { test : /\.sass$/,
-        use : ExtractTextPlugin.extract({
-          fallback : 'style-loader',
-          use : [
+const appConfig = {
+    entry: ['babel-polyfill', './src/sass/stApp.sass','./src/js/stApp.js'],
+    output: {
+        path: path.resolve(__dirname, 'web'),
+        filename: './assets/js/stApp.js',
+        publicPath: '/'
+    },
+    devServer: {
+        publicPath: '/',
+        port: 8888
+    },
+    externals: {
+        materialize: 'M'
+    },
+    module: {
+        rules: [
             {
-              loader : 'css-loader',
-              options : {
-                url: false,
-                minimize: true,
-                sourceMap: true
-              }
+                test : /\.sass$/,
+                use : [
+                    {
+                        loader : 'css-loader',
+                        options : {
+                            url: false,
+                            minimize: true,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader : 'sass-loader',
+                        options : {
+                            sourceMap: true,
+                            importer : globImporter()
+                        }
+                    }
+                ]
             },
             {
-              loader : 'sass-loader',
-              options : {
-                sourceMap: true,
-                importer : globImporter()
-              }
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
             }
-          ]
+        ]
+    },
+    performance: {
+        hints: false
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: './assets/css/stApp.min.css',
+            disable: false,
+            allChunks: true
+        }),
+        new HtmlWebpackPlugin({
+            template: 'web/app.html',
+            inject : false
         })
-      },
-      { test: /\.(js)$/, exclude: /node_modules/, use: 'babel-loader' }
     ]
-  },
-  performance: { hints: false },
-  devServer: {
-    historyApiFallback: true,
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: './assets/css/stApp.min.css',
-      disable: false,
-      allChunks: true
-    }),
-    new HtmlWebpackPlugin({
-      template: 'web/app.html',
-      inject : false
-    })
-  ]
-};
+}
+
+module.exports = [appConfig]
