5ed3c39474e7329db02babf715920601f2232807
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-04 17:01:56 -0700

    MEySdekv1DdBentnEJMd

--- src/js/components/signup/methods.js
+++ src/js/components/signup/methods.js
@@ -26,6 +26,7 @@ export default function createAccount(e) {
 
 // initSession
 export function initSession(plan) {
+    _st.loading = true
     var planId = (typeof plan === 'string') ? plan : plan.target.id.replace('stPlan-',''),
         thePlan = {}
 
