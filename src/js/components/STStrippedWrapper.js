670a2eee98ff3faba7dcdf57c48d09dd486fa710
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-05 13:18:30 -0700

    NTUS6eIfWw2ciahwUJeF

--- src/js/components/STStrippedWrapper.js
+++ src/js/components/STStrippedWrapper.js
@@ -2,19 +2,16 @@ import React from 'react'
 
 const STStrippedWrapper = ({error,children}) => {
     return (
-        <section id="stStripped" className="row">
+        <section id="stStripped">
             <div id="stStrippedInset" className="col s12 m8 l6 offset-m2 offset-l3">
-                <div id="stStrippedSpacer" className="row z-depth-2">
+                <div id="stStrippedSpacer" className="row">
                     <header className="stBordered row">
-                        <div id="stStrippedInsetDarkHeader" className="col s12">
-                            <img src="/assets/img/sttv_logo_contrast.png" />
+                        <div id="stStrippedInsetHeader" className="col s12">
+                            <img src="/assets/img/sttv_logo.png" />
                         </div>
                     </header>
                     {children}
                     <div id="stFormErrors" className="row">{error.message}</div>
-                    <footer className="row">
-                        <img src="/assets/img/supertutortv-students.png" />
-                    </footer>
                 </div>
             </div>
         </section>
