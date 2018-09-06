3f625a2628e323f7d35145ac76a81279d3c8daa3
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-06 13:39:26 -0700

    I7kee08IQpRazddC4IRj

--- src/js/components/Signup.js
+++ src/js/components/Signup.js
@@ -56,12 +56,12 @@ export default class Signup extends React.Component {
         return(
             <STSignupWrapper error={this.state.error}>
                 <Header shadow="0"/>
+                <div id="stSignupDiagSep" className="row">
+                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" fill="#ffffff" width="100%" height="100px" viewBox="0 0 1920 100" preserveAspectRatio="none">
+                        <polygon points="0,0 0,100 1920,0 "></polygon>
+                    </svg>
+                </div>
                 <div id="stSignupWrapper" className="row">
-                    <div id="stSignupDiagSep" className="row">
-                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" fill="#ffffff" width="100%" height="100px" viewBox="0 0 1920 100" preserveAspectRatio="none">
-                            <polygon points="0,0 0,100 1920,0 "></polygon>
-                        </svg>
-                    </div>
                     <SignupStep 
                         changeStep={this.changeStep} 
                         createAccount={this.createAccount} 
