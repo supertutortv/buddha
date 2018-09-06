cb40da9307cefac4283a2845a4c858510a7475ef
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-06 16:47:04 -0700

    t2BR53JlRsxKDW6YuEt2

--- src/js/components/signup/steps.js
+++ src/js/components/signup/steps.js
@@ -38,29 +38,31 @@ const Plans = ({ initSession }) => {
 
 const Account = ({ createAccount, updateInp }) => {
     return (
-        <form id="step-2" className="stFormStep row" onSubmit={createAccount}>
-            <div className="stFormHeader col s12">
-                <h2>Awesome! Let's create your account!</h2>
-                <span>Create your account below. Don't worry, we do not and will not abuse, misuse, or sell your information. Read our <a target="_blank" href="https://supertutortv.com/privacy-policy">privacy policy</a> for more info.</span>
+        <div id="stSignupAccount" class="stSignupStep row">
+            <div class="stSignupInner col s12 m6 offset-m3">
+                <form id="step-2" className="stFormStep row" onSubmit={createAccount}>
+                    <div id="stSignupAccount" className="stFormBody col s12">
+                        <div className="input-field required col s12 m6 st-input-half-left">
+                            <input className="browser-default validate" type="text" name="customer|account|firstname" placeholder="First Name" onBlur={updateInp} required />
+                        </div>
+                        <div className="input-field required col s12 m6 st-input-half-right">
+                            <input className="browser-default validate" type="text" name="customer|account|lastname" placeholder="Last Name" onBlur={updateInp} required/>
+                        </div>
+                        <div className="input-field required col s12">
+                            <input className="browser-default validate email" type="email" name="customer|account|email" placeholder="Email Address" onBlur={updateInp} required/>
+                        </div>
+                        <div className="input-field required col s12">
+                            <input className="browser-default validate" type="password" name="customer|account|password" placeholder="Password" onBlur={updateInp} required/>
+                        </div>
+                    </div>
+                    <div className="stFormButtons col s12">
+                        <button id="stBtn_account" type="submit" className="stFormButton btn waves-effect waves-light" >Next >></button>
+                    </div>
+                </form>
             </div>
-            <div id="stSignupAccount" className="stFormBody col s12">
-                <div className="input-field required col s12 m6 st-input-half-left">
-                    <input className="browser-default validate" type="text" name="customer|account|firstname" placeholder="First Name" onBlur={updateInp} required />
-                </div>
-                <div className="input-field required col s12 m6 st-input-half-right">
-                    <input className="browser-default validate" type="text" name="customer|account|lastname" placeholder="Last Name" onBlur={updateInp} required/>
-                </div>
-                <div className="input-field required col s12">
-                    <input className="browser-default validate email" type="email" name="customer|account|email" placeholder="Email Address" onBlur={updateInp} required/>
-                </div>
-                <div className="input-field required col s12">
-                    <input className="browser-default validate" type="password" name="customer|account|password" placeholder="Password" onBlur={updateInp} required/>
-                </div>
-            </div>
-            <div className="stFormButtons col s12">
-                <button id="stBtn_account" type="submit" className="stFormButton pmt-button btn waves-effect waves-light" >Next >></button>
-            </div>
-        </form>
+            <h1>Create your account.</h1><br/>
+            <span>Create your account below. Don't worry, we do not and will not abuse, misuse, or sell your information. Read our <a target="_blank" href="https://supertutortv.com/privacy-policy">privacy policy</a> for more info.</span>
+        </div>
     )
 }
 
