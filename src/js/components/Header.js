2892ad3c675974c37f43d47cca05f67bae54b18c
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-06 09:58:49 -0700

    PUsDW0ZIqGGhU0Vb2iGM

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -7,7 +7,7 @@ export default class Header extends React.Component {
 
     render() {
         return(
-            <header id="stAppHeader" className="row z-depth-1">
+            <header id="stAppHeader" className="row z-depth-3">
                 <img src={_st.root+'/assets/img/sttv_logo.png'} />
             </header>
         )
