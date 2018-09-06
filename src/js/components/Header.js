0046a4ebfadbf05ed75c87966a050eb2ec089841
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-06 13:05:19 -0700

    nXKaIsP6NeLO6RjKAVN2

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -9,7 +9,7 @@ export default class Header extends React.Component {
         let depth = this.props.shadow || "3"
         return(
             <header id="stAppHeader" className={"row z-depth-"+depth}>
-                <div id="stHeaderLeft" className="col s12 m2"></div>
+                <div id="stHeaderLeft" className="col s12 m2 hide-on-small-only"></div>
                 <div id="stHeaderMiddle" className="col s12 m8">
                     <img src={_st.root+'/assets/img/sttv_logo.png'} />
                 </div>
