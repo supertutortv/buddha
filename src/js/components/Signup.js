e93e210fead5dbafb4c21c832095a1a222213c36
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-06 11:00:30 -0700

    ZKT9qGh3YIzgF2aMOkKx

--- src/js/components/Signup.js
+++ src/js/components/Signup.js
@@ -56,9 +56,9 @@ export default class Signup extends React.Component {
         return(
             <STSignupWrapper error={this.state.error}>
                 <Header />
-                <div className="row">
+                <div id="stSignupDiagSep" className="row">
                     <svg xmlns="http://www.w3.org/2000/svg" version="1.1" fill="#ffffff" width="100%" height="90px" viewBox="0 0 1920 90" preserveAspectRatio="none">
-                        <polygon class="fil0" points="1920,0 0,90 1920,90 "></polygon>
+                        <polygon class="fil0" points="1920,0 90,0 1920,90 "></polygon>
                     </svg>
                 </div>
                 <div id="stSignupWrapper" className="stFormWrapper row">
