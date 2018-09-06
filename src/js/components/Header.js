800ce1154e585aad86e5d30481781c4d47821217
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-06 10:42:42 -0700

    chjtCyPV5hgxEkERziEr

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -8,7 +8,11 @@ export default class Header extends React.Component {
     render() {
         return(
             <header id="stAppHeader" className="row z-depth-3">
-                <img src={_st.root+'/assets/img/sttv_logo.png'} />
+                <div id="stHeaderLeft" className="col s12 m2"></div>
+                <div id="stHeaderMiddle" className="col s12 m8">
+                    <img src={_st.root+'/assets/img/sttv_logo.png'} />
+                </div>
+                <div id="stHeaderRight" className="col s12 m2"></div>
             </header>
         )
     }
