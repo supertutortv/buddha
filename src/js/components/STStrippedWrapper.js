c398bc56376198146f8f60ed55faf631da9f8fa9
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-06 17:44:37 -0700

    XONeYCJMM0XWVgljxDgA

--- src/js/components/STStrippedWrapper.js
+++ src/js/components/STStrippedWrapper.js
@@ -3,7 +3,7 @@ import React from 'react'
 const STStrippedWrapper = ({error,children}) => {
     return (
         <section id="stStripped" className="row">
-            <div id="stStrippedInset" className="col s12 m6 l4 offset-m3 offset-l4">
+            <div id="stStrippedInset" className="col s12 m6 offset-m3">
                 <div id="stStrippedSpacer" className="col s12">
                     <header className="stBordered row">
                         <div id="stStrippedInsetHeader" className="col s12">
