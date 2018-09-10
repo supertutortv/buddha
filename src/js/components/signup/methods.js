0171736d43ff32e5c73a69b7ddd8d9e9e9e5288a
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-10 16:26:07 -0700

    vYF6pxWoeYGFzLC4Wqyc

--- src/js/components/signup/methods.js
+++ src/js/components/signup/methods.js
@@ -90,7 +90,7 @@ export function setChecker(el) {
         }
 
         if (val !== this.state.pricing[p].value)
-            _st.http.get('/signup/check?'+p+'='+val+'&sig='+this.state.signature, (d) => {
+            _st.http.get('/signup/check?'+p+'='+val+'&sig='+this.state.session.signature, (d) => {
                 if (d.code === 'signup_error') {
                     tar.classList.add('invalid')
                     this.setState({error: {id: d.code, message: d.message}})
