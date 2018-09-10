a2ff3ecb2b987dbbba914a5862dd8e4f122a806e
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-10 11:53:58 -0700

    iHNaMSYSwNCZkuolPzf1

--- src/js/components/signup/pieces/PricingTable.js
+++ src/js/components/signup/pieces/PricingTable.js
@@ -4,24 +4,24 @@ const PricingTable = ({state,setChecker,toPrice}) => {
     return (
         <div id="stSignupPricingTable" className="col s12">
             <div id="stPricingHeading" className="row">
-                <div className="col s9">ITEM</div>
-                <div className="col s3 right-al">PRICE</div>
+                <div className="col s8">ITEM</div>
+                <div className="col s4 right-al">PRICE</div>
             </div>
             <div id="stPricingItems" className="row">
                 {state.items.map((item) => {
                     return (
                         <div className="item row">
-                            <div className="col s9">{item.name}</div>
-                            <div className="col s3 right-al">{toPrice(item.amt)}</div>
+                            <div className="col s8">{item.name}</div>
+                            <div className="col s4 right-al">{toPrice(item.amt)}</div>
                         </div>
                     )
                 })}
             </div>
             <div id="stPricingTotals" className="row">
-                <div className="input-field couponInp col s9">
+                <div className="input-field couponInp col s8">
                     <input class="browser-default validate coupon" name="pricing|coupon|value" type="text" placeholder="Coupon code" onBlur={setChecker} />
                 </div>
-                <div id="stPricingTotal" className="col s3 right-al">{'$'+toPrice(state.pricing.total)}</div>
+                <div id="stPricingTotal" className="col s4 right-al">{'$'+toPrice(state.pricing.total)}</div>
             </div>
         </div>
     )
