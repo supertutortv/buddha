bb587b72faaabcf1e1d659feb3b25c32a75ed1f5
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-10 09:35:18 -0700

    bdNKWZeZH34tuU11Us4T

--- src/js/components/signup/pieces/PricingTable.js
+++ src/js/components/signup/pieces/PricingTable.js
@@ -4,13 +4,15 @@ const PricingTable = ({state}) => {
     return (
         <div id="stSignupPricingTable" className="col s12">
             <div id="stPricingHeading" className="row">
-                <div className="col s9">Item</div>
-                <div className="col s3">Price</div>
+                <div className="col s9">ITEM</div>
+                <div className="col s3">PRICE</div>
             </div>
             <div id="stPricingItems" className="row">{console.log(state)}</div>
             <div id="stPricingTotals" className="row">
-                <div className="col s9">Coupon</div>
-                <div className="col s3">Total</div>
+                <div className="input-field col s9">
+                    <input class="browser-default coupon" name="pricing|coupon|value" type="text" placeholder="Coupon code" onBlur={setChecker} />
+                </div>
+                <div id="stPricingTotal" className="col s3">$0</div>
             </div>
         </div>
     )
