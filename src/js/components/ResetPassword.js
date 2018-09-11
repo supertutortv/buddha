32ad9cf24ca2daf96dc54e0ef1fbb2ca7cacdcea
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-11 16:05:48 -0700

    3Acrn8V8tLGwfWJSxKjc

--- src/js/components/ResetPassword.js
+++ src/js/components/ResetPassword.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import STStrippedWrapper from './STStrippedWrapper'
 
+const SendForm = () => {
+
+}
+
+const ResetForm = () => {
+
+}
+
 export default class ResetPassword extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
             sent: false,
+            creds: {},
             error: {
                 id: '',
                 message: ''
             }
         }
+
+        this.sendReset = this.sendReset.bind(this)
     }
 
     componentDidMount() {
@@ -20,10 +31,26 @@ export default class ResetPassword extends React.Component {
         _st.loading = false
     }
 
+    componentDidUpdate() {
+        _st.loading = false
+    }
+
+    sendReset(e) {
+        console.log(e.target)
+        /* _st.http.post('/auth/reset',this.state.customer.account,(d) => {
+            if (d.code === 'signupError') return this.setState({
+                error: {
+                    id: d.code,
+                    message: d.message
+                }
+            })
+        }) */
+    }
+
     render() {
         if (this.props.match.params.key) return null
         return (
-            <form id="stPasswordWrapper" className="stFormWrapper row">
+            <form id="stPasswordWrapper" className="stFormWrapper row" onSubmit={this.sendReset}>
                 <div id="stPasswordHeader" className="stFormHeader col s12">
                     <h1>Reset your password</h1>
                 </div>
