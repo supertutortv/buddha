045f8e7f3a57c5f67bc74208f07b97e25e191641
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-10 19:45:14 -0700

    IUqZuNx3tWZN7Jb8wYKr

--- src/js/components/signup/steps.js
+++ src/js/components/signup/steps.js
@@ -78,10 +78,10 @@ const Account = ({ createAccount, updateInp, error }) => {
 const ThankYou = ({state}) => {
     return (
         <div id="stSignupThankYou" class="stSignupStep row">
-            <div class="stSignupInner col s12">
+            <div class="stSignupInner stFormWrapper col s12">
                 <h1>Thank you!</h1>
                 <div className="row">Your order code is <strong>{state.thankYou.id}</strong>. Keep this for your records.</div>
-                <div className="row">You will receive a payment receipt when your card is charged, either after your trial expires or very soon if you skipped the trial. You will also receive a welcome email with instructions for using the course. Or if you'd like to get started right now, just click the button below.</div>
+                <div className="col s12 m6 offset-m3"><span>You will receive a payment receipt when your card is charged, either after your trial expires or very soon if you skipped the trial. You will also receive a welcome email with instructions for using the course. Or if you'd like to get started right now, just click the button below.</span></div>
                 <div className="stFormButtons row">
                     <button id="stBtn_thankyou" type="submit" className="stFormButton btn waves-effect waves-light" onClick={() => <Redirect to="/dashboard" />} >Go to dashboard</button>
                 </div>
