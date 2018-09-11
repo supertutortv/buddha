9a197cdb17844ee640af10e491be93caacb868dd
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-11 14:45:36 -0700

    Vr3MzmsS6K2D6EBSyeLd

--- src/js/components/ResetPassword.js
+++ src/js/components/ResetPassword.js
@@ -3,18 +3,7 @@ import React from 'react'
 
 /* const ResetPassword = ({setLoginState}) => 
     (<div>
-        <div id="stLoginHeader" className="stFormHeader col s12">
-            <h2>Reset your password</h2>
-            <span>Please enter the email address associated with your account</span>
-        </div>
-        <div id="stLoginCredentials" className="col s12">
-            <div className="input-field col s12">
-                <input className="browser-default validate email" type="email" name="username" placeholder="Email Address" onBlur={setLoginState}/>
-            </div>
-        </div>
-        <div className="stFormButtons col s12">
-            <button className="stFormButton btn waves-effect waves-light">Reset your password</button>
-        </div>
+        
     </div>) */
 
 export default class ResetPassword extends React.Component {
@@ -22,10 +11,29 @@ export default class ResetPassword extends React.Component {
         super(props)
     }
 
+    componentDidMount() {
+        //if (this.props.match.params.key)
+
+        _st.loading = false
+    }
+
     render() {
-        console.log(this.props)
+        if (this.props.match.params.key) return null
         return (
-            <div>Hallo worruld!</div>
+            <React.Fragment>
+            <div id="stLoginHeader" className="stFormHeader col s12">
+                <h2>Reset your password</h2>
+                <span>Please enter the email address associated with your account</span>
+            </div>
+            <div id="stLoginCredentials" className="col s12">
+                <div className="input-field col s12">
+                    <input className="browser-default validate email" type="email" name="username" placeholder="Email Address" />
+                </div>
+            </div>
+            <div className="stFormButtons col s12">
+                <button className="stFormButton btn waves-effect waves-light">Reset your password</button>
+            </div>
+            </React.Fragment>
         )
     }
 }
