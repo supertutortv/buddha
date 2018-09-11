1ab811c016cd69594429f6097db0be441cb3475e
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-11 15:26:59 -0700

    4H4r9lfbtJtKwbE0Pp53

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import STStrippedWrapper from '../STStrippedWrapper'
+import STStrippedWrapper from './STStrippedWrapper'
 import { LoginForm } from './login/Forms'
 import * as methods from './login/methods'
 
