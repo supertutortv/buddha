35a1b291f924dcd29249d40bd0c27cfa08717f5b
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-11 17:47:31 -0700

    DdgEKvdQTYRu9Z5zLKOp

--- src/js/components/ResetPassword.js
+++ src/js/components/ResetPassword.js
@@ -38,7 +38,7 @@ export default class ResetPassword extends React.Component {
     sendReset(e) {
         e.preventDefault()
         var formData = new FormData(e.target)
-        console.log(formData.getAll())
+        console.log(formData)
         /* _st.http.post('/auth/reset',this.state.customer.account,(d) => {
             if (d.code === 'signupError') return this.setState({
                 error: {
