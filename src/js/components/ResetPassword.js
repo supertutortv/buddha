794abd5498a501060afc1e20d828ad7d7debe641
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-12 16:29:41 -0700

    9uF05v4iar5OvwbsGGd7

--- src/js/components/ResetPassword.js
+++ src/js/components/ResetPassword.js
@@ -79,7 +79,7 @@ export default class ResetPassword extends React.Component {
     componentDidMount() {
         var obj = {}
         _st.bodyClass = 'passwordReset'
-        if (typeof this.state.key !== 'undefined' && this.state.key !== null)
+        if (this.state.key !== null)
             console.log('check token')
             _st.http.get('/auth/reset?key='+this.props.match.params.key,(d) => {
                 if (d.code === 'pwError')
