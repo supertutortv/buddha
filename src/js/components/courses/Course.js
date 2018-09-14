ab62baa5a8a4a745d6bd66b6efccc4ab987d2295
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-14 13:37:26 -0700

    6oEBns9ppscMiUSPpMWE

--- src/js/components/courses/Course.js
+++ src/js/components/courses/Course.js
@@ -1,24 +1,15 @@
 import React from 'react'
 import {Switch,Route,Redirect} from 'react-router-dom'
 
-export default class Course extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            activeObj : {}
-        }
-
-        /* let {params} = this.props.match
-        Object.keys(params) */
-    }
-
-    componentDidMount() {}
-
-    render() {
-        if (this.state.activeObj === null) return null
-        return(
-            <div>{JSON.stringify(this.props.match)}</div>
-        )
-    }
+const Course = (props) => {
+    console.log(props)
+    return(
+        <DataState.Consumer>
+            {(data) => {
+                return null
+            }}
+        </DataState.Consumer>
+    )
 }
+
+export default Course
