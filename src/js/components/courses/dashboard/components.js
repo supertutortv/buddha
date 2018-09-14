79d8e8a5a673afcc5f4cab9f3106affcd9cba517
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-14 13:10:40 -0700

    Wo9hNVRaJo8iPssZwlwQ

--- src/js/components/courses/dashboard/components.js
+++ src/js/components/courses/dashboard/components.js
@@ -4,12 +4,12 @@ import { Switch, Route, Redirect, Link } from 'react-router-dom'
 export const DBStats = () => 
     <div className="stDashboardStats row">Stats row</div>
 
-export const DBCourses = ({courses}) => {
+export const DBCourses = ({data}) => {
     return (
         <div className="stDashboardCourses row">
-            {Object.keys(courses).map((course) =>
+            {Object.keys(data).map((course) =>
                 <div>
-                    <Link to={'/'+course} />
+                    <Link to={'/'+course} >{data[course].name}</Link>
                 </div>
             )}
         </div>
