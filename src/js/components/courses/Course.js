a47a2ee96188c5ab6cf3a91a36e6d5ee0f9a6949
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-14 14:39:50 -0700

    JSCnX9siSElC98m38wmb

--- src/js/components/courses/Course.js
+++ src/js/components/courses/Course.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { DataState } from './StateContext'
+import ST404 from '../ST404'
 import {Switch,Route,Redirect} from 'react-router-dom'
 
-const Course = (props) => {
-    console.log(props)
+const Course = ({location: loc, history: hist, match, setState}) => {
+
     return(
         <DataState.Consumer>
             {(data) => {
-                return null
+                try {
+                    let course = data.courses[match.params.course]
+                    return (
+                        <div>{JSON.stringify(course)}</div>
+                    )
+                } catch (e) {
+                    <ST404 />
+                }
             }}
         </DataState.Consumer>
     )
