a893b2781aa3a89e0e878179b6ab12ce92f4cd0f
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-18 15:42:14 -0700

    J3p66dFtmNlBKuAanGOy

--- src/js/components/Playlist.js
+++ src/js/components/Playlist.js
@@ -3,6 +3,10 @@ import React from 'react'
 export default class Playlist extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            obj: this.props.obj
+        }
     }
 
     componentDidMount() {}
@@ -11,7 +15,9 @@ export default class Playlist extends React.Component {
         return (
             <section className="stPlaylistRoot">
                 <div className="stPlaylistInner">
-                    <div className="stPlaylistColA"></div>
+                    <div className="stPlaylistColA">
+                        <figure className="stVideoStage"></figure>
+                    </div>
                     <div className="stPlaylistColB">{JSON.stringify(this.props.obj)}</div>
                 </div>
             </section>
