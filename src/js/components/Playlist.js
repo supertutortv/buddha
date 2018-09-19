3cd863dee9ddee3f948bc08bc24588031cda8380
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-19 13:27:40 -0700

    SSpfid6gYsa4XmMQZLFr

--- src/js/components/Playlist.js
+++ src/js/components/Playlist.js
@@ -74,7 +74,7 @@ export default class Playlist extends React.Component {
                             </div>
                             <figcaption className="stVideoCaption">
                                 <div className="stVideoTitle">
-                                    <h1>{obj.name ? obj.name+' - ' : ''}<span>{' (video title)'}</span></h1>
+                                    <h1>{obj.name ? obj.name+' - ' : ''}<span>{vid.name || ''}</span></h1>
                                 </div>
                             </figcaption>
                         </figure>
