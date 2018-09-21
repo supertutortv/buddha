74b20de6eaaf963bffd23c223b4d013fc8c09c68
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-21 16:06:07 -0700

    VqpUf17T3Rp91XkEXRSL

--- src/js/components/courses/STSectionBox.js
+++ src/js/components/courses/STSectionBox.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const STSectionBox = ({icon = 'atom',title = 'boosh',color = '#109fda'}) => 
-    <div className="stSectionBox">
+    <div className="stSectionBox" style={{backgroundColor:color}}>
         <div className="stSectionBoxInner z-depth-3">
             <i className={"brainy-"+icon}></i>
             <div>{title}</div>
