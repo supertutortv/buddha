2830e321e9de33f2fab1d7bf2a59ccac03e43cf0
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-22 13:53:45 -0700

    J53mvX51P0hhVSM1V8U1

--- src/js/components/courses/STSectionBox.js
+++ src/js/components/courses/STSectionBox.js
@@ -2,20 +2,18 @@ import React from 'react'
 import {Switch,Route,Redirect,Link} from 'react-router-dom'
 
 const STSectionBox = ({slug, description, icon = 'atom', title, color}) =>
-    <Link to={slug}>
-        <section className="stSectionBoxWrap">
-            <div className="stSectionBox">
-                <div className="stSectionBoxInner" style={{border:'3px solid '+color}}>
-                    <div className="stSectionBoxContainer">
-                        <div className="boxHeader" style={{backgroundColor:color}}><i className={"brainy-"+icon}></i></div>
-                        <div className="boxText">
-                            <div className="boxTitle">{title}</div>
-                            <div className="boxDesc">{description}</div>
-                        </div>
+    <section className="stSectionBoxWrap">
+        <div className="stSectionBox">
+            <div className="stSectionBoxInner" style={{border:'3px solid '+color}}>
+                <div className="stSectionBoxContainer">
+                    <div className="boxHeader" style={{backgroundColor:color}}><i className={"brainy-"+icon}></i></div>
+                    <div className="boxText">
+                        <div className="boxTitle">{title}</div>
+                        <div className="boxDesc">{description}</div>
                     </div>
                 </div>
             </div>
-        </section>
-    </Link>
+        </div>
+    </section>
 
 export default STSectionBox
