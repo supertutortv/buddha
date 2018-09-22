55eafc10e46d59c4e268cafb6bbed0e1b561ea0f
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-22 10:54:23 -0700

    FLD050TNZ4ybGyGR3Tqq

--- src/js/components/courses/STSectionBox.js
+++ src/js/components/courses/STSectionBox.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-const STSectionBox = ({icon = 'atom',title = 'boosh',color = '#109fda'}) => 
-    <div className="stSectionBox">
-        <div className="stSectionBoxInner z-depth-3" style={{backgroundColor:color}}>
-            <div className="stSectionBoxContainer">
-                <i className={"brainy-"+icon}></i>
-                <div>{title}</div>
+const STSectionBox = ({icon = 'atom',title = 'boosh',color = '#109fda'}) =>
+    <section className="stSectionBoxWrap">
+        <div className="stSectionBox">
+            <div className="stSectionBoxInner z-depth-3" style={{backgroundColor:color}}>
+                <div className="stSectionBoxContainer">
+                    <div>
+                        <div><i className={"brainy-"+icon}></i></div>
+                        <div>{title}</div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 
 export default STSectionBox
