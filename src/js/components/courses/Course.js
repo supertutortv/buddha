4fbd2be43e1a3ecff2572fb53388ab3ffd29a301
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-22 15:04:53 -0700

    KeWpKeYhkshf8FKdkg7u

--- src/js/components/courses/Course.js
+++ src/js/components/courses/Course.js
@@ -30,7 +30,7 @@ const Course = ({location: loc, history: hist, match, setState}) => {
                             collections = data.courses[params.courses].collections
                         Object.keys(collections).forEach((val) => {
                             if (val === 'practice') return
-                            sections.push(<STSectionBox hist={hist} slug={val} {...collections[val]} icon="owl" />)
+                            sections.push(<STSectionBox hist={hist} path={loc.pathname+'/'+val} {...collections[val]} icon="owl" />)
                         })
 
                         return (
