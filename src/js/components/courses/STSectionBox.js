ae029c23a4bcbcf9fe83398608ef7a7c3f6c6bf4
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-22 13:00:59 -0700

    flRHMsOx70kskTvDxT5S

--- src/js/components/courses/STSectionBox.js
+++ src/js/components/courses/STSectionBox.js
@@ -3,10 +3,10 @@ import React from 'react'
 const STSectionBox = ({desc = '', icon = 'atom',title = 'boosh',color = '#109fda'}) =>
     <section className="stSectionBoxWrap">
         <div className="stSectionBox">
-            <div className="stSectionBoxInner z-depth-3" style={{border:'3px solid '+color}}>
+            <div className="stSectionBoxInner" style={{border:'3px solid '+color}}>
                 <div className="stSectionBoxContainer">
                     <div className="boxHeader" style={{backgroundColor:color}}><i className={"brainy-"+icon}></i></div>
-                    <div>
+                    <div className="boxText">
                         <div className="boxTitle">{title}</div>
                         <div className="boxDesc">This is where the Section description will go</div>
                     </div>
