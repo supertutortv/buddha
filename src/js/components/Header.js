cf297d46a71ba10361287cbaa4f4ac8e7898ebe5
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-25 15:34:54 -0700

    MqPq9drA35cF6Sz8iliM

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -4,10 +4,8 @@ import PropTypes from 'prop-types'
 const Header = ({depth, courseNav}) => {
     return(
         <header id="stAppHeader" className={"row z-depth-"+depth}>
-            <div id="stHeaderLeft" className="col s12 m2 hide-on-small-only"></div>
-            <div id="stHeaderMiddle" className="col s12 m8">
-                <img src={_st.root+'/assets/img/sttv_logo.png'} />
-            </div>
+            <div id="stHeaderLeft" className="col s12 m2 hide-on-small-only"><img src={_st.root+'/assets/img/sttv_logo.png'} /></div>
+            <div id="stHeaderMiddle" className="col s12 m8"></div>
             <div id="stHeaderRight" className="col s12 m2">
                 {!courseNav || <div></div>}
             </div>
