c3ab3fa0f6a78bc7658e8830620b7740baee39aa
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-26 11:59:16 -0700

    jGdS0kleT3A95izmnL3y

--- src/js/components/FAIco.js
+++ src/js/components/FAIco.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faSignOut } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
-library.add(fab, fas)
+library.add(fab, faUser, faSignOut)
 
 const FAIco = props => <FontAwesomeIcon {...props} />
 
