56500e57b8b9243c1f6c120c70463336a3d82956
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-26 11:45:10 -0700

    r17FrDGDq26MlgU16gee

--- src/js/components/FAIco.js
+++ src/js/components/FAIco.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
