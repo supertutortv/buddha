54a69f7898212c61eb2da016a053d450fc1a746d
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-26 09:39:24 -0700

    xdDBbpmNbn3rbdrkXAut

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,27 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {Switch,Route,Redirect,Link} from 'react-router-dom'
 
-const CourseNav = () => {
-    return (
-    <ul>
-        <li><a href="http://support.supertutortv.com" target="blank"><i className="brainy-question-mark"></i></a></li>
-        <li><a href="#" onClick={(e) => {
-            e.preventDefault()
-            return (
-                <Redirect push to="/dashboard" />
-            )
-        }} ><i className="brainy-pupil"></i></a></li>
-    </ul>
-    )
-}
-
 const Header = ({depth}) => {
     return(
         <header id="stAppHeader" className={"row z-depth-"+depth}>
             <div id="stHeaderLeft" className="col s12 m2 hide-on-small-only"><img src={_st.root+'/assets/img/sttv_logo.png'} /></div>
             <div id="stHeaderMiddle" className="col s12 m8"></div>
             <div id="stHeaderRight" className="col s12 m2">
-                <CourseNav />
+                <ul>
+                    <li><a href="http://support.supertutortv.com" target="blank"><i className="brainy-question-mark"></i></a></li>
+                    <li><a href="#" onClick={(e) => {
+                        e.preventDefault()
+                        return (
+                            <Redirect push to="/dashboard" />
+                        )
+                    }} ><i className="brainy-pupil"></i></a></li>
+                </ul>
             </div>
         </header>
     )
