3a4ae2734b2a9b1613c51a0b4a7063317f6afb0a
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-26 13:54:26 -0700

    kalyHKFCcsGhaVHt45dP

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -4,10 +4,10 @@ import FAIco from './FAIco'
 
 const Header = ({title, depth, hist}) => {
     return(
-        <header id="stAppHeader" className={"row z-depth-"+depth}>
-            <div id="stHeaderLeft" className="col s12 m2 hide-on-small-only"><img src={_st.root+'/assets/img/sttv_logo.png'} /></div>
-            <div id="stHeaderMiddle" className="col s12 m8"><h1>{title}</h1></div>
-            <div id="stHeaderRight" className="col s12 m2">
+        <header className={"stAppHeader z-depth-"+depth}>
+            <div className="stHeaderLeft"><img src={_st.root+'/assets/img/sttv_logo.png'} /></div>
+            <div className="stHeaderMiddle"><h1>{title}</h1></div>
+            <div className="stHeaderRight">
                 <ul className="stNavContainer">
                     <li><FAIco icon="user" onClick={(e) => {
                         e.preventDefault()
