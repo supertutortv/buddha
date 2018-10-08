969150e0f5c772990d3f1bf9c1195e87e84f56fa
David Paul <enlightenedpie@users.noreply.github.com>
2018-10-08 16:54:37 -0700

    cIYpihalNCqRQqOXIOE1

--- src/js/components/modal/comps.js
+++ src/js/components/modal/comps.js
@@ -6,31 +6,26 @@ const Downloads = ({data}) => {
         <div className="stModalDownloads">
             <h1>Downloads</h1>
             <div className="stModalDownloadsContainer">
-                {data.map(x => {
-                    
-                    if (x.length > 0) return (
-                        <section className="stDownloadSection">
-                            <div className="stDownloadLinks">
-                                {x.map(f => {
-                                    let emp = (f.file === 0),
-                                        path = emp || f.file.split('/'),
-                                        href = emp ? null : 'https://api.supertutortv.com/course-dl.php?test='+path[0]+'&section='+path[1]+'&res='+path[2]+'&hash='+f.hash
-                                    return (
-                                        <figure className={"stDownloadLink"+(emp ? ' disabled' : '')} onClick={(e) => {
-                                            if (emp)
-                                                return e.preventDefault()
-                                            else
-                                                window.location = href
-                                        }}>
-                                            <img src={f.thumb}/>
-                                            <figcaption>{f.name}</figcaption>
-                                        </figure>
-                                    )
-                                })}
-                            </div>
-                        </section>
-                    )
-                })}
+                <section className="stDownloadSection">
+                    <div className="stDownloadLinks">
+                        {data.map(f => {
+                            let emp = (f.file === 0),
+                                path = emp || f.file.split('/'),
+                                href = emp ? null : 'https://api.supertutortv.com/course-dl.php?test='+path[0]+'&section='+path[1]+'&res='+path[2]+'&hash='+f.hash
+                            return (
+                                <figure className={"stDownloadLink"+(emp ? ' disabled' : '')} onClick={(e) => {
+                                    if (emp)
+                                        return e.preventDefault()
+                                    else
+                                        window.location = href
+                                }}>
+                                    <img src={f.thumb}/>
+                                    <figcaption>{f.name}</figcaption>
+                                </figure>
+                            )
+                        })}
+                    </div>
+                </section>
             </div>
         </div>
     )
