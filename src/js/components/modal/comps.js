ead7849492d38b278e0df67b946655825272a405
David Paul <enlightenedpie@users.noreply.github.com>
2018-10-08 16:52:58 -0700

    PtXAcgiM9JqlekMRXSUS

--- src/js/components/modal/comps.js
+++ src/js/components/modal/comps.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Downloads = ({data}) => {
-    console.log(dls)
+    console.log(data)
     return (
         <div className="stModalDownloads">
             <h1>Downloads</h1>
