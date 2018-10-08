d012b0b2c2a171240fe34c019aac3f681275c530
David Paul <enlightenedpie@users.noreply.github.com>
2018-10-08 12:43:53 -0700

    0jMHj8vicvG7x1O3jem8

--- src/js/components/modal/comps.js
+++ src/js/components/modal/comps.js
@@ -9,6 +9,14 @@ const Downloads = ({data}) => {
                     if (x.files.length > 0) return (
                         <section className="stDownloadSection">
                             <h2>{x.name}</h2>
+                            <div className="stDonwloadLinks">
+                                {x.files.map(f => {
+                                    let emp = (f.file === 0)
+                                    return (
+                                        <a href={emp || f.file} className={"stDownloadLink"+(emp ? ' disabled' : '')} onClick={(e) => {if (emp) e.preventDefault()}}>{f.name}</a>
+                                    )
+                                })}
+                            </div>
                         </section>
                     )
                 })}
