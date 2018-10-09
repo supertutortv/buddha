2014bf92b93f2b87187304c8ab959b65ef673082
David Paul <enlightenedpie@users.noreply.github.com>
2018-10-08 17:34:39 -0700

    f0y1ZyNuzwjlf5iSKsbX

--- src/js/components/modal/comps.js
+++ src/js/components/modal/comps.js
@@ -5,28 +5,24 @@ const Downloads = ({data}) => {
     return (
         <div className="stModalDownloads">
             <h1>Downloads</h1>
-            <div className="stModalDownloadsContainer">
-                <section className="stDownloadSection">
-                    <div className="stDownloadLinks">
-                        {data.map(f => {
-                            let emp = (f.file === 0),
-                                path = emp || f.file.split('/'),
-                                href = emp ? null : 'https://api.supertutortv.com/course-dl.php?test='+path[0]+'&section='+path[1]+'&res='+path[2]+'&hash='+f.hash
-                            return (
-                                <figure className={"stDownloadLink"+(emp ? ' disabled' : '')} onClick={(e) => {
-                                    if (emp)
-                                        return e.preventDefault()
-                                    else
-                                        window.location = href
-                                }}>
-                                    <img src={f.thumb}/>
-                                    <figcaption>{f.name}</figcaption>
-                                </figure>
-                            )
-                        })}
-                    </div>
-                </section>
-            </div>
+            <section className="stDownloadLinks">
+                {data.map(f => {
+                    let emp = (f.file === 0),
+                        path = emp || f.file.split('/'),
+                        href = emp ? null : 'https://api.supertutortv.com/course-dl.php?test='+path[0]+'&section='+path[1]+'&res='+path[2]+'&hash='+f.hash
+                    return (
+                        <figure className={"stDownloadLink"+(emp ? ' disabled' : '')} onClick={(e) => {
+                            if (emp)
+                                return e.preventDefault()
+                            else
+                                window.location = href
+                        }}>
+                            <img src={f.thumb}/>
+                            <figcaption>{f.name}</figcaption>
+                        </figure>
+                    )
+                })}
+            </section>
         </div>
     )
 }
