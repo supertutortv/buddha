213f544fe620be04aa7688f9f89aa7efc0916103
David Paul <enlightenedpie@users.noreply.github.com>
2018-10-10 17:12:22 -0700

    3klsjupXekbmZMPabu1u

--- src/js/components/Playlist.js
+++ src/js/components/Playlist.js
@@ -105,7 +105,7 @@ export default class Playlist extends React.Component {
     updateUdata(patch,vid,test,loc) {
         this.state.updating = true
         _st.udata.update(patch,Object.assign(vid,{test:test,path:loc.pathname}),(d) => {
-            console.log(d.data)
+            console.log(d)
             this.setState({updating: false})
         })
     }
