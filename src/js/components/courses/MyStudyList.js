4b71e081e2e8ac9127670a2116806d6cf8dc3f85
David Paul <enlightenedpie@users.noreply.github.com>
2018-10-29 11:43:49 -0700

    tenLmPWpyQKSWSngF1M6

--- src/js/components/courses/MyStudyList.js
+++ src/js/components/courses/MyStudyList.js
@@ -4,7 +4,13 @@ import FAIco from '../FAIco'
 const MyStudyList = ({data}) => {
     let list = []
 
-    data.forEach((i) => list.push(<div>{JSON.stringify(i)}</div>))
+    data.forEach((i) => list.push(
+        <div className="stCourseStudyListItem">
+            <picture>
+                <img src={"https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F"+i.thumb+"_295x166.jpg&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png"} />
+            </picture>
+        </div>
+    ))
 
     return (
         <div className="stCourseMSL">
