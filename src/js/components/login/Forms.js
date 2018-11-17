c14eb91de5d6d19cf080e9acccd4416b38a10fba
David Paul <enlightenedpie@users.noreply.github.com>
2018-11-17 14:59:26 -0800

    IWdCwTKP9sFGbSKDOCRP

--- src/js/components/login/Forms.js
+++ src/js/components/login/Forms.js
@@ -2,21 +2,21 @@ import React from 'react'
 
 const LoginForm = ({setLoginState, lostPwGo}) =>
     (<div>
-        <div id="stLoginHeader" className="stFormHeader col s12">
+        <div id="stLoginHeader" className="stFormHeader">
             <h1>Welcome! Please sign in.</h1>
         </div>
-        <div id="stLoginCredentials" className="col s12">
-            <div className="input-field col s12">
+        <div className="stLoginCredentials">
+            <div className="input-field">
                 <input className="browser-default validate email" type="email" name="username" placeholder="Email Address" onBlur={setLoginState}/>
             </div>
-            <div className="input-field col s12">
+            <div className="input-field">
                 <input className="browser-default validate" type="password" name="password" placeholder="Password" onBlur={setLoginState}/>
             </div>
         </div>
-        <div className="stFormButtons col s12">
+        <div className="stFormButtons">
             <button className="stFormButton btn waves-effect waves-light">Sign In</button>
         </div>
-        <div className="stForgotBlock col s12">
+        <div className="stForgotBlock">
             <span><a onClick={lostPwGo}>Forgot your password?</a></span>
         </div>
     </div>)
