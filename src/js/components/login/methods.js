44444530c6625bd5d7c5a9c3cb26ea089724c799
David Paul <enlightenedpie@users.noreply.github.com>
2018-11-17 15:20:11 -0800

    oWg5feDGGktEdHwugXSn

--- src/js/components/login/methods.js
+++ src/js/components/login/methods.js
@@ -22,6 +22,7 @@ export function setLoginState(e) {
 
 export function submit(e) {
     e.preventDefault()
+    return console.log(e)
     _st.loading = true
     _st.auth.submit('/auth/token',this.state.creds,(d) => {
         switch (d.code) {
