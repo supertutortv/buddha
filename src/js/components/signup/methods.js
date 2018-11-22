bbaa2d00bb171a694a42c3b5aaa741044bee8fb4
David Paul <enlightenedpie@users.noreply.github.com>
2018-11-22 15:41:01 -0800

    fLV4mmCXimDLt0UtLPRk

--- src/js/components/signup/methods.js
+++ src/js/components/signup/methods.js
@@ -113,7 +113,7 @@ export function setShipping(el) {
         pricing: Object.assign(this.state.pricing,{shipping: el.target.checked ? parseInt(this.state.item.metadata.priship) : 0})
     })
     this.updateInp(el)
-    this.calculateShipping()
+    this.calculatePricing()
     console.log(this.state)
 }
 
