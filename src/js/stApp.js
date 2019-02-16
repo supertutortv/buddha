8278c79184747b920ebcc844fc7a706cb6d7fcaf
David Paul <enlightenedpie@gmail.com>
2019-02-16 16:55:44 -0600

    Testing out global header

--- src/js/stApp.js
+++ src/js/stApp.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import allYourBase from './components/allYourBase'
 import Signup from './components/Signup'
+import Header from './components/Header'
 import MU from './components/MU'
 import ResetPassword from './components/ResetPassword'
 import STSecured from './components/STSecured'
@@ -16,6 +17,7 @@ window._st = STTV
 
 ReactDOM.render( 
     <BrowserRouter>
+        <Header />
         <Switch>
             <Route exact path='/all-your-base-are-belong-to-us' component={allYourBase} />
             <Route exact path='/signup/:plan?' component={Signup} />
