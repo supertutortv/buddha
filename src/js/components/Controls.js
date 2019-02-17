ddf4c516a2db7dba0dfcd58155cf57ba64393609
David Paul <enlightenedpie@gmail.com>
2019-02-16 20:38:34 -0600

    Testing new components

---
+++ src/js/components/Controls.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Controls extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div className="stControlBar"></div>
+		)
+	}
+}
--- src/js/components/STApp.js
+++ src/js/components/STApp.js
@@ -3,24 +3,29 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import allYourBase from './allYourBase'
 import Signup from './Signup'
 import Header from './Header'
+import Controls from './Controls'
 import MU from './MU'
 import ResetPassword from './ResetPassword'
 import STSecured from './STSecured'
 
 export default class STApp extends React.Component {
+	constructor(props) {
+		super(props)
+	}
 
 	render() {
 		return (
 			<React.Fragment>
-			<Header />
-			<Switch>
-				<Route exact path='/all-your-base-are-belong-to-us' component={allYourBase} />
-				<Route exact path='/signup/:plan?' component={Signup} />
-				<Route exact path='/mu/:teacher' component={MU} />
-				<Route exact path='/password/reset/:key?' component={ResetPassword} />
-				<Route exact path='/login' component={STSecured} />
-				<Route path='/' render={(p) => <STSecured {...p} />} />
-			</Switch>
+				<Controls />
+				<Header />
+				<Switch>
+					<Route exact path='/all-your-base-are-belong-to-us' component={allYourBase} />
+					<Route exact path='/signup/:plan?' component={Signup} />
+					<Route exact path='/mu/:teacher' component={MU} />
+					<Route exact path='/password/reset/:key?' component={ResetPassword} />
+					<Route exact path='/login' component={STSecured} />
+					<Route path='/' render={(p) => <STSecured {...p} />} />
+				</Switch>
 			</React.Fragment>
 		)
 	}
