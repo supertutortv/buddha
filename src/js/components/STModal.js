3badd3cdedd1fe4ce83bc4337023066ab3813094
David Paul <enlightenedpie@gmail.com>
2019-03-03 20:55:02 -0600

    forced re-render for adding modal type class after first render (for transition effects)

--- src/js/components/STModal.js
+++ src/js/components/STModal.js
@@ -1,13 +1,32 @@
 import React from 'react'
 
-const STModal = ({type = 'default', children, closer = () => {}}) => {
-    return (
-        <div className={['stModal',type].join(' ')} onClick={(e) => e.target.classList.contains("stModal") && closer()}>
-            <div className="stModalInner">
-                {children}
+class STModal extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            typeClass: ''
+        }
+    }
+
+    componentDidMount() {
+        this.setState({typeClass:this.props.type})
+    }
+
+    render() {
+        let { typeClass } = this.state
+        return (
+            <div className={['stModal',typeClass].join(' ')} onClick={(e) => e.target.classList.contains("stModal") && closer()}>
+                <div className="stModalInner">
+                    {children}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+}
+
+STModal.defaultProps = {
+    type: 'default'
 }
 
 export default STModal
