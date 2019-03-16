ffd49171654bcb5504306425e6a478270edb44d9
David Paul <enlightenedpie@gmail.com>
2019-03-16 17:47:27 -0500

    Updating modal styles

--- src/js/components/login/Forms.js
+++ src/js/components/login/Forms.js
@@ -3,7 +3,7 @@ import React from 'react'
 const LoginForm = ({setLoginState, lostPwGo}) =>
     <React.Fragment>
         <div id="stLoginHeader" className="stFormHeader">
-            <h1>Welcome! Please sign in.</h1>
+            <h1>Welcome! Please sign in</h1>
         </div>
         <div className="stLoginCredentials">
             <div className="input-field">
@@ -12,12 +12,12 @@ const LoginForm = ({setLoginState, lostPwGo}) =>
             <div className="input-field">
                 <input className="browser-default validate" type="password" name="password" placeholder="Password" onBlur={setLoginState}/>
             </div>
-        </div>
-        <div className="stFormButtons">
-            <button className="stFormButton btn waves-effect waves-light">Sign In</button>
             <div className="stForgotBlock">
                 <span><a onClick={lostPwGo}>Forgot your password?</a></span>
             </div>
+        </div>
+        <div className="stFormButtons">
+            <input type="submit" value="Sign In" className="stFormButton btn waves-effect waves-light"/>
         </div>
     </React.Fragment>
 
