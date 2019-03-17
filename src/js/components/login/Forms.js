131e03462c11745646339257cce8fe9bbaba94ca
David Paul <enlightenedpie@gmail.com>
2019-03-16 20:33:02 -0500

    moved error block higher in the login form

--- src/js/components/login/Forms.js
+++ src/js/components/login/Forms.js
@@ -13,10 +13,10 @@ const LoginForm = ({submit, error, setLoginState, lostPwGo}) =>
             <div className="input-field">
                 <input className="browser-default validate" type="password" name="password" placeholder="Password" onBlur={setLoginState}/>
             </div>
+            {error.message ? <div className="stFormErrors">{error.message}</div> : null}
             <div className="stForgotBlock">
                 <span><a onClick={lostPwGo}>Forgot your password?</a></span>
             </div>
-            {error.message ? <div className="stFormErrors">{error.message}</div> : null}
         </div>
         <div className="stFormButtons">
             <a href="#" className="stFormButton btn" onClick={submit}><em>Sign In</em><FAIco icon="sign-in-alt"/></a>
