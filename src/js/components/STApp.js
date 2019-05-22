33b4ced3e08235678a9dd662c7a9e114660f0ba6
David Paul <enlightenedpie@gmail.com>
2019-05-21 22:59:02 -0500

    devpush from remote

--- src/js/components/STApp.js
+++ src/js/components/STApp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import allYourBase from './allYourBase'
 import Gateway from './Gateway'
 import Signup from './Signup'
@@ -10,8 +10,29 @@ export default class STApp extends React.Component {
         super(props)
 
         this.state = {
-            something: true
+            loggedIn: null
         }
+
+        this.logThatFuckerIn = this.logThatFuckerIn.bind(this)
+        this.authCheck = this.authCheck.bind(this)
+    }
+
+    authCheck() {
+        if (this.state.loggedIn === null) {
+            _st.http.post('/auth/verify',{},(d) => {
+                console.log(d.data)
+                this.setState({
+                    loggedIn: d.data
+                })
+            })
+        }
+        return null
+    }
+
+    logThatFuckerIn() {
+        this.setState({
+            loggedIn: true
+        })
     }
 
     render () {
@@ -19,11 +40,11 @@ export default class STApp extends React.Component {
             <BrowserRouter>
                 <Switch>
                     <Route exact path='/all-your-base-are-belong-to-us' component={allYourBase} />
-                    {/* <Route exact path='/signup/:plan?' component={Signup} />
-                    <Route exact path='/password/reset/:key?' component={ResetPassword} /> */}
-                    <Route exact path={['/login','/password/reset/:key?','/signup/:plan?']} render={(p) => {
-                        console.log(this.state.something)
-                        return null
+                    {/* <Route exact path='/signup/:plan?' component={Signup} /> */}
+                    <Route exact path={['/auth','/login','/password/reset/:key?','/signup/:plan?']} render={(p) => {
+                        console.log(p)
+                        if (this.state.loggedIn === null) return this.authCheck()
+
                     }} />
                     <Route path='/' render={(p) => <Gateway {...p} />} />
                 </Switch>
