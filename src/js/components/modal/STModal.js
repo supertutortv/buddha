bf08a26f588117f0db04155fc7e126ee24b9ee93
David Paul <enlightenedpie@gmail.com>
2019-05-24 18:13:42 -0500

    devpush from remote

--- src/js/components/modal/STModal.js
+++ src/js/components/modal/STModal.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import * as comps from './comps'
 
-const STModal = ({ mData, open, action, orientation = 'bottom', addDl, refr, modalActive, color, test, children }) => {
+const STModal = (props) => {
+    let { open } = props
     //if (!open) return null
 
-        const ModalComp = comps[action] || ''
+    let { xtraClass, mData, action, orientation = 'bottom', addDl, refr, modalActive, color, test, children } = this.props
 
-        var style = color ? {borderColor: color} : {}
-        
-        return (
-            <div className={['stModal',orientation].join(' ')} onClick={(e) => {
-                if (e.target.classList.contains("stModal")) modalActive({open: false})
-            }}>
-                <div className="stModalInner" style={style}>
-                    {children || <ModalComp test={test} refDls={refr} reportDl={addDl} color={color} data={mData} />}
-                </div>
+    const ModalComp = comps[action] || children || ''
+
+    var style = color ? {borderColor: color} : {}
+    
+    return (
+        <aside className={['stModal',orientation, xtraClass].join(' ')} onClick={(e) => {
+            if (e.target.classList.contains("stModal")) modalActive({open: false})
+        }}>
+            <div className="stModalInner" style={style}>
+                <ModalComp test={test} refDls={refr} reportDl={addDl} color={color} data={mData} />
             </div>
-        )
+        </aside>
+    )
 }
 
 export default STModal
