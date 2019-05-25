bad5dd3215f257083796c1a4354a12f1d7843d29
David Paul <enlightenedpie@gmail.com>
2019-05-24 21:02:49 -0500

    devpush from remote

--- src/js/components/Checkout.js
+++ src/js/components/Checkout.js
@@ -70,7 +70,7 @@ export default class Checkout extends React.Component {
 
     render() {
         if (!this.state.init) return null
-        console.log(this.state.plan)
+        console.log(this.props.plan)
         return(
             <AuthContext.Consumer>
                 {auth => {
