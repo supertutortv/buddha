e69a6d04f20dcf5f1ab3494c7960124d9d31ff68
David Paul <enlightenedpie@gmail.com>
2019-05-26 22:07:37 -0500

    devpush from remote

--- src/js/components/Checkout.js
+++ src/js/components/Checkout.js
@@ -61,7 +61,6 @@ export default class Checkout extends React.Component {
         return(
             <AuthContext.Consumer>
                 {auth => {
-                    console.log(auth)
                     return (
                         <StripeProvider stripe={this.state.stripe}>
                             <Elements>
