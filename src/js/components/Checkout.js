a82de66ded5f39d06e5083e7d081fc18015836d2
David Paul <enlightenedpie@gmail.com>
2019-05-28 09:41:51 -0500

    devpush from remote

--- src/js/components/Checkout.js
+++ src/js/components/Checkout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StripeProvider, Elements, CardElement } from 'react-stripe-elements'
-import crypto from 'crypto-js/core'
+import crypto from 'crypto'
 import Buttons from './checkout/Buttons'
 import methods from './checkout/methods'
 import * as steps from './checkout/steps'
