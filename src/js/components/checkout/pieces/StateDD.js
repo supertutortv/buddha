ea5cf16f590fe607b81684618c6b8e01da6c0fd0
David Paul <enlightenedpie@gmail.com>
2019-06-08 22:49:40 -0500

    devpush from remote

--- src/js/components/checkout/pieces/StateDD.js
+++ src/js/components/checkout/pieces/StateDD.js
@@ -5,6 +5,7 @@ const StateDD = ({children,...props}) => {
     return (
 		<React.Fragment>
         <select {...props}>
+			<option disabled selected>State...</option>
 			<option value="AL">Alabama</option>
 			<option value="AK">Alaska</option>
 			<option value="AZ">Arizona</option>
