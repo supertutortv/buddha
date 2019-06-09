0c56a79a6249e3d1030f7567dfa17341cd6e9c9d
David Paul <enlightenedpie@gmail.com>
2019-06-08 19:58:28 -0500

    devpush from remote

--- src/js/components/onboarding/Onboarding.js
+++ src/js/components/onboarding/Onboarding.js
@@ -52,12 +52,12 @@ const Shipping = ({nextStep,ship}) => {
 						else
 							nextStep({step: 3, loc: e.target.value, shipping: false})
 					}} required />
-					{!ship ? null : <form className="shippingForm nopad">
-						<div className="stIfR99 twoq right">
+					{!ship ? null : <form className="shippingForm nopad big">
+						<div className="stIfR99 twoq left">
 							<input aria-label="Address Line 1" className="validate address" type="text" name="line1" required validation="text"/>
 							<label aria-hidden="true" for="line1">Address Line 1</label>
 						</div>
-						<div className="stIfR99 twoq left">
+						<div className="stIfR99 twoq right">
 							<input aria-label="Address Line 2" className="address" type="text" name="line2"/>
 							<label aria-hidden="true" for="line2">Address Line 2</label>
 						</div>
