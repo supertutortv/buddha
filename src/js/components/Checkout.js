d006748e3170c3672a7cf3d4be695296c742939f
David Paul <enlightenedpie@gmail.com>
2019-06-15 21:46:01 -0500

    devpush from remote

--- src/js/components/Checkout.js
+++ src/js/components/Checkout.js
@@ -169,7 +169,7 @@ export default class Checkout extends React.Component {
                                         this.setState({card: el})
                                     }}/>
                                 </div>
-                                {!state.doTrial ? null : <div className="stTrialMsg">You elected the 5 day limited free trial. You card will not be charged until {theDate.setDate(theDate.getDate() + 5)}. By clicking below, you agree to pay the full amount due to unlock your course subscription.</div>}
+                                {!state.doTrial ? null : <div className="stTrialMsg">You elected the 5 day limited free trial. You card will not be charged until {theDate.setDate(theDate.getDate() + 5).toString()}. By clicking below, you agree to pay the full amount due to unlock your course subscription.</div>}
                                 <div className="stSubmitBlock">
                                     <button id="paySubmit" name="paySubmit" type="submit" className={active}>
                                         <span>{disabled ? 'Processing...' : (completed) ? '' : 
