9833421d4e862016c7f8c6efe56faa157bc96b07
David Paul <enlightenedpie@gmail.com>
2019-08-26 13:27:57 -0500

    pushing changes from dev

--- src/js/components/courses/MyStudyList.js
+++ src/js/components/courses/MyStudyList.js
@@ -64,7 +64,7 @@ export default class MyStudyList extends React.Component {
                     <div className="stCourseResources">
                         <a target="_blank" href="#" onClick={(e) => {
                             e.preventDefault()
-                            console.log(e.target)
+                            console.log(e.currentTarget)
                         }}>
                         <i class="fas fa-link"></i>
                         <span>Course Resources</span>
