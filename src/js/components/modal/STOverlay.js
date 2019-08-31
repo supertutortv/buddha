6f1536ce5c724510eb003449df53994d810ebc7d
David Paul <enlightenedpie@gmail.com>
2019-08-31 13:19:26 -0500

    pushing changes from dev

--- src/js/components/modal/STOverlay.js
+++ src/js/components/modal/STOverlay.js
@@ -2,10 +2,7 @@ import React from 'react'
 import STRings from '../../svg/STRings'
 
 export default ({close=()=>{},className='default', children}) => (
-    <st-overlay class={className} onClick={(e) => {
-        e.preventDefault()
-        close()
-    }}>
+    <st-overlay class={className} onClick={close}>
         <div onClick={e => e.stopPropagation()}>
             {children || <STRings/>}
         </div>
