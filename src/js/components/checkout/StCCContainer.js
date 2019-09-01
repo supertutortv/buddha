b5c91d8e9fda27efd31ca1c2f1086da20a734b63
David Paul <enlightenedpie@gmail.com>
2019-09-01 15:09:55 -0500

    pushing changes from dev

--- src/js/components/checkout/StCCContainer.js
+++ src/js/components/checkout/StCCContainer.js
@@ -3,7 +3,26 @@ import React from 'react'
 export default ({card}) => {
     console.log(card)
 
+    const cardIcons = {
+        'Visa': <i class="fab fa-cc-visa"></i>,
+        'MasterCard': <i class="fab fa-cc-mastercard"></i>,
+        'American Express': <i class="fab fa-cc-amex"></i>,
+        'Discover': <i class="fab fa-cc-discover"></i>,
+        'Diners Club': <i class="fab fa-cc-diners-club"></i>,
+        'default': <i class="fas fa-credit-card"></i>
+    }
+
+    let Icon = (card.brand in cardIcons) ? cardIcons[card.brand] : cardIcons['default']
+
     return (
-        <st-cc-container></st-cc-container>
+        <st-cc-container>
+            <div>
+                <p><Icon/></p>
+                <p>
+                    <span>{card.name}</span><br/>
+                    <span>°°°° {card.last4}</span>
+                </p>
+            </div>
+        </st-cc-container>
     )
 }
