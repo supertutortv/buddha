31888f0441528ca7f59f0f53bf62468f74828a40
David Paul <enlightenedpie@gmail.com>
2019-09-01 21:10:25 -0500

    pushing changes from dev

--- src/js/components/checkout/StCCContainer.js
+++ src/js/components/checkout/StCCContainer.js
@@ -1,25 +1,59 @@
 import React from 'react'
+import {injectStripe, CardElement} from 'react-stripe-elements'
 
-export default ({card}) => {
+const cardIcons = {
+    'Visa': <i class="fab fa-cc-visa"></i>,
+    'MasterCard': <i class="fab fa-cc-mastercard"></i>,
+    'American Express': <i class="fab fa-cc-amex"></i>,
+    'Discover': <i class="fab fa-cc-discover"></i>,
+    'Diners Club': <i class="fab fa-cc-diners-club"></i>,
+    'default': <i class="fas fa-credit-card"></i>
+}
 
-    const cardIcons = {
-        'Visa': <i class="fab fa-cc-visa"></i>,
-        'MasterCard': <i class="fab fa-cc-mastercard"></i>,
-        'American Express': <i class="fab fa-cc-amex"></i>,
-        'Discover': <i class="fab fa-cc-discover"></i>,
-        'Diners Club': <i class="fab fa-cc-diners-club"></i>,
-        'default': <i class="fas fa-credit-card"></i>
-    }
+const _stcont = ({card}) => {
+    let stripeYuh = null
 
     return (
         <st-cc-container>
-            <div id="activCardEl">
-                <p className="ccBrand">{(card.brand in cardIcons) ? cardIcons[card.brand] : cardIcons['default']}</p>
-                <p className="ccDetails">
-                    <span>{card.name}</span><br/>
-                    <span className="last4">•••• {card.last4}</span>
-                </p>
-            </div>
+            <StripeProvider stripe={stripeYuh}>
+                <Elements>
+                    <div id="activCardEl">
+                        {stripeYuh === null ?
+                            <>
+                                <p className="ccBrand">{(card.brand in cardIcons) ? cardIcons[card.brand] : cardIcons['default']}</p>
+                                <p className="ccDetails">
+                                    <span>{card.name}</span><br/>
+                                    <span className="last4">•••• {card.last4}</span>
+                                </p>
+                            </> :
+                        <CardElement id="stripeInject" onChange={(d) => {
+                            if (typeof d !== 'undefined') {
+                                if (typeof d.error !== 'undefined') return this.setState({
+                                    cardComplete: false,
+                                    error: {
+                                        id: 'checkoutError',
+                                        message: d.error.message
+                                    }
+                                })
+
+                                if (d.complete && !d.empty) this.setState({
+                                    cardComplete: true,
+                                    error: {
+                                        id: '',
+                                        message: ''
+                                    }
+                                })
+                            }
+                        }} onReady={(el) => {
+                            this.setState({card: el})
+                        }}/>}
+                    </div>
+                </Elements>
+            </StripeProvider>
         </st-cc-container>
     )
 }
+
+const StCCContainer = injectStripe(_stcont)
+
+export default StCCContainer
