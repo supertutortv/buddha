e401279abc7e8c8c98de69b790437379391ccade
David Paul <enlightenedpie@gmail.com>
2019-09-01 21:50:13 -0500

    pushing changes from dev

--- src/js/components/checkout/StCCContainer.js
+++ src/js/components/checkout/StCCContainer.js
@@ -17,42 +17,44 @@ export default ({card}) => {
         <st-cc-container>
             <StripeProvider stripe={stripeYuh}>
                 <Elements>
-                    <div id="activCardEl">
-                        {stripeYuh === null ?
-                            <>
-                                <p className="ccBrand">{(card.brand in cardIcons) ? cardIcons[card.brand] : cardIcons['default']}</p>
-                                <p className="ccDetails">
-                                    <span>{card.name}</span><br/>
-                                    <span className="last4">•••• {card.last4}</span>
-                                </p>
-                            </> :
-                        <CardElement id="stripeInject" onChange={(d) => {
-                            if (typeof d !== 'undefined') {
-                                if (typeof d.error !== 'undefined') return this.setState({
-                                    cardComplete: false,
-                                    error: {
-                                        id: 'checkoutError',
-                                        message: d.error.message
-                                    }
-                                })
+                    <>
+                        <div id="activCardEl">
+                            {stripeYuh === null ?
+                                <>
+                                    <p className="ccBrand">{(card.brand in cardIcons) ? cardIcons[card.brand] : cardIcons['default']}</p>
+                                    <p className="ccDetails">
+                                        <span>{card.name}</span><br/>
+                                        <span className="last4">•••• {card.last4}</span>
+                                    </p>
+                                </> :
+                            <CardElement id="stripeInject" onChange={(d) => {
+                                if (typeof d !== 'undefined') {
+                                    if (typeof d.error !== 'undefined') return this.setState({
+                                        cardComplete: false,
+                                        error: {
+                                            id: 'checkoutError',
+                                            message: d.error.message
+                                        }
+                                    })
 
-                                if (d.complete && !d.empty) this.setState({
-                                    cardComplete: true,
-                                    error: {
-                                        id: '',
-                                        message: ''
-                                    }
-                                })
-                            }
-                        }} onReady={(el) => {
-                            this.setState({card: el})
-                        }}/>}
-                    </div>
-                    <div className="newCard">
-                        <a href="#" onClick={(e) => e.preventDefault()}>
-                            + add new card
-                        </a>
-                    </div>
+                                    if (d.complete && !d.empty) this.setState({
+                                        cardComplete: true,
+                                        error: {
+                                            id: '',
+                                            message: ''
+                                        }
+                                    })
+                                }
+                            }} onReady={(el) => {
+                                this.setState({card: el})
+                            }}/>}
+                        </div>
+                        <div className="newCard">
+                            <a href="#" onClick={(e) => e.preventDefault()}>
+                                + add new card
+                            </a>
+                        </div>
+                    </>
                 </Elements>
             </StripeProvider>
         </st-cc-container>
