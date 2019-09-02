244df6c88d5365789b26d436ccfa28df4420b3dd
David Paul <enlightenedpie@gmail.com>
2019-09-01 22:52:49 -0500

    pushing changes from dev

--- src/js/components/checkout/StCCContainer.js
+++ src/js/components/checkout/StCCContainer.js
@@ -27,7 +27,7 @@ export default ({card}) => {
     
             document.body.appendChild(s)
         } else {
-            setStripeYuh(window.Stripe)
+            setStripeYuh(window.Stripe(_st.stripe))
         }
     }
 
