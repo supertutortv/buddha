967af5bb5cd436b2736a666a07970f8a8fd049c4
David Paul <enlightenedpie@gmail.com>
2019-09-15 19:38:29 -0500

    pushing changes from dev

--- src/js/components/ST404.js
+++ src/js/components/ST404.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ST404 = () => {
     _st.bodyClass = 'notFound'
     return (
-        <div style={{backgroundColor:'yellow',height:'100%',width:'100%'}}>Not found</div>
+        <div style={{backgroundColor:'whitesmoke',height:'100%',width:'100%'}}>Not found</div>
     )
 }
 
