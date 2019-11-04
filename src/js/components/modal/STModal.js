76e1155081737d6826501f1e11d31beec84c5c0a
David Paul <enlightenedpie@gmail.com>
2019-11-03 22:04:40 -0600

    pushing changes for merge

--- src/js/components/modal/STModal.js
+++ src/js/components/modal/STModal.js
@@ -7,16 +7,16 @@ const STModal = (props) => {
 
     let { dismissable, xtraClass, mData, action, actData, orientation = 'bottom', addDl, refr, modalActive, color, test, children } = props
 
-    const closer = dismissable ? modalActive({
-        open: false
-    }) : null
-
     const ModalComp = comps[action] || '',
         style = color ? {borderColor: color} : {}
     
     return (
-        <aside className={['stModal',orientation, xtraClass].join(' ')} onClick={closer}>
-            <div className="stModalInner" style={style} onClick={e => e.stopPropagation()}>
+        <aside className={['stModal',orientation, xtraClass].join(' ')} onClick={(e) => {
+            if (dismissable) modalActive({
+                open: false
+            })
+        }}>
+            <div onClick={e => e.stopPropagation()} className="stModalInner" style={style}>
                 {children || <ModalComp test={test} refDls={refr} reportDl={addDl} color={color} data={mData || actData} />}
             </div>
         </aside>
